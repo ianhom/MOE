3403d025182a3e45a890f69765c4a0ed7a695f35
Ian Hom <ianhom@163.com>
2016-05-03 17:39:42 +0800

    Create

---
+++ Pub/common.h
@@ -0,0 +1,33 @@
+
+/******************************************************************************
+* File       : common.h
+* Function   : common definitions.
+* description: To be done.           
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd May 2016
+* History    :  No.  When           Who           What
+*               1    3/May/2016     Ian           Create
+******************************************************************************/
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Definition of process function for tasks */
+typedef uint16 (*PF_TASK_PROCESS)(uint16 u16Evt);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _COMMON_H_ */
+
+/* End of file */
+
