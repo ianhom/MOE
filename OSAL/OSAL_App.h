f3b630228a61d95ab381c855dd1608dc56cc10c2
Ian Hom <ianhom@163.com>
2016-05-03 17:38:56 +0800

    Create

---
+++ OSAL/OSAL_App.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+* File       : OSAL_App.h
+* Function   : User application definitions.
+* description: To be done.           
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd May 2016
+* History    :  No.  When           Who           What
+*               1    3/May/2016     Ian           Create
+******************************************************************************/
+
+#ifndef _OSAL_APP_H_
+#define _OSAL_APP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define MAX_TASK_NUM             (5)      /* Max number of tasks */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OSAL_APP_H */
+
+/* End of file */
+
