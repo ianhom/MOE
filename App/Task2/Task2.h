c7e4f9c683a885edaf335ee103498e5b91cb8125
Ian Hom <ianhom@163.com>
2016-05-04 17:25:46 +0800

    Create

---
+++ App/Task2/Task2.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+* File       : Task2.h
+* Function   : To be doen.
+* description: To be done.          
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd May 2016
+* History    :  No.  When           Who           What
+*               1    3/May/2016     Ian           Create
+******************************************************************************/
+
+#ifndef _TASK_2_H_
+#define _TASK_2_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void Task2_Init(uint8 u8TaskID);
+uint16 Task2_Process(uint16 u16Evt);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _TASK_2_H */
+
+/* End of file */
+
+
