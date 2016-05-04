b56c02f53fdbabf4ac9f4b3acfec1feda14eea31
Ian Hom <ianhom@163.com>
2016-05-04 17:22:00 +0800

    Create

---
+++ App/Task1/Task1.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+* File       : OSAL_App.h
+* Function   : User application definitions.
+* description: To be done.          
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd May 2016
+* History    :  No.  When           Who           What
+*               1    3/May/2016     Ian           Create
+******************************************************************************/
+
+#ifndef _TASK_1_H_
+#define _TASK_1_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void Task1_Init(uint8 u8TaskID);
+uint16 Task1_Process(uint16 u16Evt);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _TASK_1_H */
+
+/* End of file */
+
+
