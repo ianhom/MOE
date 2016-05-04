3c3ade42ed0c0e29b6602086616b35b7a3158a22
Ian Hom <ianhom@163.com>
2016-05-04 17:26:37 +0800

    Create

---
+++ App/Task3/Task3.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+* File       : Task3.h
+* Function   : To be doen.
+* description: To be done.          
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd May 2016
+* History    :  No.  When           Who           What
+*               1    3/May/2016     Ian           Create
+******************************************************************************/
+
+#ifndef _TASK_3_H_
+#define _TASK_3_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void Task3_Init(uint8 u8TaskID);
+uint16 Task3_Process(uint16 u16Evt);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _TASK_3_H */
+
+/* End of file */
+
+
