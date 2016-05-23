e35380e5ef65417278a4eeaa2b6921fd39081c23
Ian Hom <ianhom@163.com>
2016-05-23 22:41:16 +0800

    Update Task2.c

--- App/Task2/Task2.c
+++ App/Task2/Task2.c
@@ -55,7 +55,6 @@ uint16 Task2_Process(uint16 u16Evt)
     if (u16Evt & EVENT_TEST)
     {
         DBG_PRINT("This is task 2 process function, Task ID is %d\n", sg_u8TaskID);
-        Osal_Event_Set(2,EVENT_TEST);
         return (u16Evt ^ EVENT_TEST);
     }
 
