4bb7b4df8f834a7726eae91617768c558f7b4328
Ian Hom <ianhom@163.com>
2016-05-28 17:21:09 +0800

    Update OSAL_Msg.h

--- OSAL/OSAL_Msg.h
+++ OSAL/OSAL_Msg.h
@@ -43,6 +43,37 @@ typedef struct _T_MSG_HEAD
 }T_MSG_HEAD;
 
 
+/******************************************************************************
+* Name       : void* Osal_Msg_Create(uint16 u16Size,uint8 u8MsgType)
+* Function   : Create a message
+* Input      : uint16 u16Size       0~65535   Length of the Message    
+*              uint8  u8MsgType     0~255     Type of message
+* Output:    : None
+* Return     : Pointer of the message data struct.
+*              NULL:  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 26th May 2016
+******************************************************************************/
+void* Osal_Msg_Create(uint16 u16Size,uint8 u8MsgType);
+
+/******************************************************************************
+* Name       : uint8 Osal_Msg_Send(uint8 u8DestTask,T_MSG_HEAD *ptMsg)
+* Function   : Create a message
+* Input      : uint8       u8DestTask   0~254    The destination task number
+*              T_MSG_HEAD *ptMsg                 The pointer of message 
+* Output:    : None
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 28th May 2016
+******************************************************************************/
+uint8 Osal_Msg_Send(uint8 u8DestTask,T_MSG_HEAD *ptMsg);
+
+
  
 #ifdef __cplusplus
 }
