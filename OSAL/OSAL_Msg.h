7ada29fee294d95a934ce5bed5629f38c94a6092
Ian Hom <ianhom@163.com>
2016-05-31 13:49:22 +0800

    Update OSAL_Msg.h

--- OSAL/OSAL_Msg.h
+++ OSAL/OSAL_Msg.h
@@ -30,9 +30,10 @@ extern "C" {
 * Structure  : T_MSG_HEAD
 * Description: Structure of message head.
 * Memebers   : void   *ptNext                 Next node of message
-*              uint16  u16Size      1~655XX   Length of message(NOT includes the head)
-*              uint8   u8DestTask   0~254     The Destination task number
+*              uint16  u16Size      1~65535   Length of message(NOT includes the head)
+*              uint8   u8DestTask   1~254     The Destination task number
 *              uint8   u8MsgType    0~255     Kind of message types
+*              uint8   u8CopyCnt    0~255     Count for message copy
 *              uint8   au8Data[0];            Data information of the message           
 *******************************************************************************/
 typedef struct _T_MSG_HEAD
@@ -41,6 +42,7 @@ typedef struct _T_MSG_HEAD
     uint16  u16Size;            /* Length of message(NOT includes the head)  */
     uint8   u8DestTask;         /* The Destination task number               */
     uint8   u8MsgType;          /* Kind of message types                     */
+    uint8   u8CopyCnt;          /* Count for message copy                    */
 #ifndef __FLEXIBLE_ARRAY_NOT_SUPPORTED
     uint8   au8Data[0];         /* Data information of the message           */
 #endif
@@ -50,7 +52,7 @@ typedef struct _T_MSG_HEAD
 /******************************************************************************
 * Name       : uint8 Osal_Msg_Send(uint8 u8DestTask,uint8 u8MsgType,uint16 u16Size,void *ptMsg)
 * Function   : Send the message to the destination task.
-* Input      : uint8  u8DestTask    0~254     The destination task number
+* Input      : uint8  u8DestTask    1~254     The destination task number
 *              uint8  u8MsgType     0~255     Type of message
 *              uint16 u16Size       0~65535   Length of the Message
 *              void *ptMsg                    Pointer of user message information
