d68ea003a5019cdcdca8eb6be5d60eed35350a2a
Ian Hom <ianhom@163.com>
2016-06-03 14:06:31 +0800

    Update OSAL_Msg.h

--- OSAL/OSAL_Msg.h
+++ OSAL/OSAL_Msg.h
@@ -23,11 +23,13 @@ extern "C" {
 #ifdef __DEBUG_MODE
 #undef __DEBUG_MODE
 #endif
-#define __DEBUG_MODE      __DEBUG_MODE_OSAL_MSG       /* According the set from project_config.h   */
+#define __DEBUG_MODE      __DEBUG_MODE_OSAL_MSG       /* According the set from project_config.h */
 #endif
 
-#define OSAL_MSG_POLL_NONE          (0)               /* Unnecessary to poll message process       */
-#define OSAL_MSG_POLL               (1)               /* Need to poll message process              */
+#define OSAL_MSG_POLL_NONE          (0)               /* Unnecessary to poll message process     */
+#define OSAL_MSG_POLL               (1)               /* Need to poll message process            */
+
+#define MSG_TYPE_TEST               (1)               /* The message type for test               */
 
 /*******************************************************************************
 * Structure  : T_MSG_HEAD
@@ -57,7 +59,7 @@ typedef struct _T_MSG_HEAD
 
 typedef struct _T_TEST_MSG
 {
-    union _DATA
+    union DATA
     {
         uint32 u32Data;
         uint16 au16Data[2];
