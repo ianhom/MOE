aee066dbf433e65788d920fc035639c95f1899fc
Ian Hom <ianhom@163.com>
2016-06-06 20:04:44 +0800

    Update Task3.c

--- App/Task3/Task3.c
+++ App/Task3/Task3.c
@@ -18,7 +18,10 @@
 #include "Task3.h"
 #include "debug.h"
 
+static uint16 Task3_Process(uint16 u16Evt);
+
 static uint8 sg_u8TaskID = TASK_NO_TASK;
+static uint8 sg_u8Cnt = 0;
 
 /******************************************************************************
 * Name       : void Task3_Init(uint8 u8TaskID)
@@ -33,12 +36,13 @@ static uint8 sg_u8TaskID = TASK_NO_TASK;
 ******************************************************************************/
 void Task3_Init(uint8 u8TaskID)
 {
-
     sg_u8TaskID = u8TaskID;        /* Get the task ID */
-
     Osal_Reg_Tasks(Task3_Process);
-
     DBG_PRINT("Task 3 is inited successfully, Task ID is %d\n", sg_u8TaskID);
+
+    /*--------------------   Add your init code here   ----------------------*/
+
+    /*-------------------   The end of your init code   ---------------------*/
     
     return;
 }
@@ -54,35 +58,51 @@ void Task3_Init(uint8 u8TaskID)
 * Author     : Ian
 * Date       : 3rd May 2016
 ******************************************************************************/
-uint16 Task3_Process(uint16 u16Evt)
+static uint16 Task3_Process(uint16 u16Evt)
 {
     uint8  u8MsgType;
     void  *ptMsg;
 
-    if (u16Evt & EVENT_TEST)
-    {
-        DBG_PRINT("This is task 3 process function, Task ID is %d\n", sg_u8TaskID);
-        return (u16Evt ^ EVENT_TEST);
-    }
-
-    if (u16Evt & EVENT_MSG)
-    {
+/******************************************************************************/
+/* Process for message event                                                  */
+/******************************************************************************/
+    EVENT_PROCESS_BEGIN(EVENT_MSG); 
+    /*-----------------   Add your event process code here   -----------------*/
         ptMsg = (void*)Osal_Msg_Receive(sg_u8TaskID, &u8MsgType);
-        switch(u8MsgType)
-        {
-            case MSG_TYPE_TEST:
+        while(ptMsg)
+        {   sg_u8Cnt++;
+            switch(u8MsgType)
             {
-                T_TEST_MSG *ptTestMsg = (T_TEST_MSG*)ptMsg;
-                DBG_PRINT("This is task %d\n",sg_u8TaskID);
-                DBG_PRINT("I get a uint32 data 0x%x!\n",(ptTestMsg->DATA.u32Data));
-                DBG_PRINT("I get a uint16 data 0x%x,0x%x!\n",(ptTestMsg->DATA.au16Data[0]),(ptTestMsg->DATA.au16Data[1]));
-                DBG_PRINT("I get a uint8 data 0x%x,0x%x,0x%x,0x%x!\n\n",(ptTestMsg->DATA.au8Data[0]),(ptTestMsg->DATA.au8Data[1]),(ptTestMsg->DATA.au8Data[2]),(ptTestMsg->DATA.au8Data[3]));
+                case MSG_TYPE_TEST:
+                {
+                    T_TEST_MSG *ptTestMsg = (T_TEST_MSG*)ptMsg;
+                    DBG_PRINT("This is task %d\n",sg_u8TaskID);
+                    DBG_PRINT("Get %d messages\n",sg_u8Cnt);
+                    //DBG_PRINT("I get a uint32 data 0x%x!\n",(ptTestMsg->DATA.u32Data));
+                    //DBG_PRINT("I get a uint16 data 0x%x,0x%x!\n",(ptTestMsg->DATA.au16Data[0]),(ptTestMsg->DATA.au16Data[1]));
+                    //DBG_PRINT("I get a uint8 data 0x%x,0x%x,0x%x,0x%x!\n\n",(ptTestMsg->DATA.au8Data[0]),(ptTestMsg->DATA.au8Data[1]),(ptTestMsg->DATA.au8Data[2]),(ptTestMsg->DATA.au8Data[3]));
+                }
             }
+            ptMsg = (void*)Osal_Msg_Receive(sg_u8TaskID, &u8MsgType);
         }
-        return (u16Evt ^ EVENT_MSG);
-    }
+    /*----------------  The end of your event process code  ------------------*/
+    EVENT_PROCESS_END(EVENT_MSG);
+/******************************************************************************/
 
-    return u16Evt;
+
+/******************************************************************************/
+/* Process for test event                                                     */
+/******************************************************************************/
+    EVENT_PROCESS_BEGIN(EVENT_TEST);
+    /*-----------------   Add your event process code here   -----------------*/
+
+    DBG_PRINT("This is task %d process function\n", sg_u8TaskID);
+
+    /*----------------  The end of your event process code  ------------------*/
+    EVENT_PROCESS_END(EVENT_TEST);
+/******************************************************************************/
+
+    return 0;
 }
 
 /* End of file */
