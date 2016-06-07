e26fff985b41b0faf393673bfc3d17fa27b116fb
Ian Hom <ianhom@163.com>
2016-06-07 23:37:15 +0800

    Update OSAL_Link_list.c

--- OSAL/OSAL_Link_list.c
+++ OSAL/OSAL_Link_list.c
@@ -16,7 +16,53 @@
 #include "OSAL_Link_List.h"
 #include "debug.h"
 
+/******************************************************************************
+* Name       : static T_TIMER_NODE* Osal_Timer_Add()
+* Function   : Add a timer node
+* Input      : PF_TIMER_SRC pfSysTm   Funtion which returns system time
+* Output:    : None
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 2nd Jun 2016
+******************************************************************************/
+void* Osal_Link_List_Add(void **pptHead, void **pptTail, void* ptNode)
+{
+    uint32 u32IntSt;
+    /* If the input node pointer is invalid  */
+    if ((NULL ==  ptNode) \
+     || (NULL ==  pptHead) \
+     || (NULL ==  pptTail) \
+     || (NULL == *pptHead) \
+     || (NULL == *pptTail))                         
+    {                        
+        DBG_PRINT("Invalid pointer when node deteling");
+        return NULL;                  /* Return NULL, no node to be added */
+    }
 
+    ptNode->ptNext = NULL;            /* Set the next node as NULL        */
+
+    ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
+    /**************************************************************************************************/
+    if(NULL == *pptTail)              /* If there is NO nodes             */
+    {
+        *pptHead = ptNode;            /* Add new node as the fisrt one    */
+        DBG_PRINT("The added node is the head one!!\n");
+    }
+    else                              /* If node exsits                   */
+    {
+        *pptTail->ptNext = ptNode;    /* Add new node after the tail one  */
+        DBG_PRINT("The added node is the tail one!!\n");
+    }
+    *pptTail = ptNode;                /* Update the tail node             */
+    /**************************************************************************************************/
+    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
+
+    return ptNode;
+
+}
 
 
 
