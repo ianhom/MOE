80750594429d3efbf1488d2022e60e0fff1b7084
Ian Hom <ianhom@163.com>
2016-06-07 23:35:50 +0800

    Create OSAL_Link_list.c

---
+++ OSAL/OSAL_Link_list.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+* File       : OSAL_Link_list.c
+* Function   : General link list function.
+* description: To be done.          
+* Version    : V1.00
+* Author     : Ian
+* Date       : 2nd Jun 2016
+* History    :  No.  When           Who           What
+*               1    02/Jun/2016    Ian           Create
+******************************************************************************/
+
+#include "type_def.h"
+#include "common_head.h"
+#include "project_config.h"
+#include "OSAL.h"
+#include "OSAL_Link_List.h"
+#include "debug.h"
+
+
+
+
+
+/* end of file */
+
