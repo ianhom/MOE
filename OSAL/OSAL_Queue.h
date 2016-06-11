2c6bf16925e091c2928ae4b85263eb9a19d8df9a
Ian Hom <ianhom@163.com>
2016-06-11 11:31:53 +0800

    Update OSAL_Queue.h

--- OSAL/OSAL_Queue.h
+++ OSAL/OSAL_Queue.h
@@ -1,4 +1,4 @@
-/*/******************************************************************************
+/******************************************************************************
 * File       : OSAL_Queue.h
 * Function   : General queue function.
 * description: To be done.          
@@ -26,8 +26,8 @@ extern "C" {
 #define __DEBUG_MODE      __DEBUG_MODE_OSAL_QUEUE         /* According the set from project_config.h */
 #endif
 
-#define OSAL_QUEUE_LAST_FREE(p)        (p->pu8Addr + p->u8End * p->u8CntMax)
-#define OSAL_QUEUE_FIRST_USED(p)       (p->pu8Addr + p->u8Begin * p->u8CntMax)
+#define OSAL_QUEUE_LAST_FREE(p)        ((p)->pu8Addr + (p)->u8End * (p)->u8MaxCnt)
+#define OSAL_QUEUE_FIRST_USED(p)       ((p)->pu8Addr + (p)->u8Begin * (p)->u8MaxCnt)
 
 
 typedef struct _T_QUEUE_INFO
@@ -39,6 +39,45 @@ typedef struct _T_QUEUE_INFO
     uint8  u8MaxCnt;
     uint8  u8Cnt;
 }T_QUEUE_INFO;
+
+
+/******************************************************************************
+* Name       : uint8 Osal_Queue_Create(uint8 u8Len, uint8 u8Num)
+* Function   : Create a queue and return the queue number
+* Input      : PF_TIMER_SRC pfSysTm   Funtion which returns system time
+* Output:    : None
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 2nd Jun 2016
+******************************************************************************/
+T_QUEUE_INFO* Osal_Queue_Create(T_QUEUE_INFO* ptQueueInfo, uint8 u8Len, uint8 u8Cnt);
+
+uint8 Osal_Queue_Inc(T_QUEUE_INFO *ptQueue);
+
+uint8 Osal_Queue_Dec(T_QUEUE_INFO *ptQueue);
+
+uint8 Osal_Queue_Is_Free(T_QUEUE_INFO *ptQueue);
+
+uint8 Osal_Queue_Is_Empty(T_QUEUE_INFO *ptQueue);
+
+
+/******************************************************************************
+* Name       : uint8 Osal_Queue_Delete(T_QUEUE_INFO* ptQueueInfo)
+* Function   : Create a queue and return the queue number
+* Input      : PF_TIMER_SRC pfSysTm   Funtion which returns system time
+* Output:    : None
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 2nd Jun 2016
+******************************************************************************/
+uint8 Osal_Queue_Delete(T_QUEUE_INFO* ptQueueInfo);
+void Osal_Queue_Test_General();
 
  
 #ifdef __cplusplus
