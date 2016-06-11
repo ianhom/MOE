738befd667a419c8e93311b308319c0228d1160c
Ian Hom <ianhom@163.com>
2016-06-11 11:31:34 +0800

    Update OSAL_Queue.c

--- OSAL/OSAL_Queue.c
+++ OSAL/OSAL_Queue.c
@@ -28,11 +28,10 @@
 * Author     : Ian
 * Date       : 2nd Jun 2016
 ******************************************************************************/
-T_QUEUE_INFO* Osal_Queue_Create(uint8 u8Len, uint8 u8Cnt)
+T_QUEUE_INFO* Osal_Queue_Create(T_QUEUE_INFO* ptQueueInfo, uint8 u8Len, uint8 u8Cnt)
 {
     uint32        u32IntSt;
     uint16        u16Size     = u8Len * u8Cnt;
-    T_QUEUE_INFO *ptQueueInfo = NULL;
 
     /* If the buffer size is 0, return NULL */
     if(0 == u16Size)
@@ -43,9 +42,12 @@ T_QUEUE_INFO* Osal_Queue_Create(uint8 u8Len, uint8 u8Cnt)
 
     ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
     /**************************************************************************************************/
-    ptQueueInfo->pu8Addr = (uint8*)OSAL_MALLOC(u16Size);
-    ptQueueInfo->u8Begin = 0;
-    ptQueueInfo->u8End   = 0;
+    ptQueueInfo->pu8Addr  = (uint8*)OSAL_MALLOC(u16Size);
+    ptQueueInfo->u8Begin  = 0;
+    ptQueueInfo->u8End    = 0;
+    ptQueueInfo->u8Len    = u8Len;
+    ptQueueInfo->u8MaxCnt = u8Cnt;
+    ptQueueInfo->u8Cnt    = 0;
     /**************************************************************************************************/
     EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */    
 
@@ -75,7 +77,7 @@ uint8 Osal_Queue_Inc(T_QUEUE_INFO *ptQueue)
     /**************************************************************************************************/
     EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */    
 
-    
+    return SW_OK;
 }
 
 uint8 Osal_Queue_Dec(T_QUEUE_INFO *ptQueue)
@@ -99,11 +101,12 @@ uint8 Osal_Queue_Dec(T_QUEUE_INFO *ptQueue)
     ptQueue->u8Cnt--;
     /**************************************************************************************************/
     EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */   
+
+    return SW_OK;
 }
 
 uint8 Osal_Queue_Is_Free(T_QUEUE_INFO *ptQueue)
 {
-    uint32        u32IntSt;
     /* If the pointer of queue info is invalid */
     if(NULL == ptQueue)
     {
@@ -122,7 +125,6 @@ uint8 Osal_Queue_Is_Free(T_QUEUE_INFO *ptQueue)
 
 uint8 Osal_Queue_Is_Empty(T_QUEUE_INFO *ptQueue)
 {
-    uint32        u32IntSt;
     /* If the pointer of queue info is invalid */
     if(NULL == ptQueue)
     {
@@ -155,7 +157,6 @@ uint8 Osal_Queue_Is_Empty(T_QUEUE_INFO *ptQueue)
 uint8 Osal_Queue_Delete(T_QUEUE_INFO* ptQueueInfo)
 {
     uint32        u32IntSt;
-    T_QUEUE_INFO *ptQueueInfo = NULL;
 
     /* If the pointer of queue information is invalid */
     if(NULL == ptQueueInfo)
@@ -175,7 +176,22 @@ uint8 Osal_Queue_Delete(T_QUEUE_INFO* ptQueueInfo)
 
 }
 
+void Osal_Queue_Test_General()
+{
+    static T_QUEUE_INFO sg_tQueue;
+    
+    Osal_Queue_Create(&sg_tQueue,5,5);
 
+    while(SW_OK == Osal_Queue_Is_Free(&sg_tQueue))
+    {
+        for(uint8 u8Idx = 0; u8Idx < 5; u8Idx++)
+        {
+            OSAL_QUEUE_LAST_FREE(&sg_tQueue)[u8Idx] = u8Idx + 1;
+        }
+        Osal_Queue_Inc(&sg_tQueue);
+    }
+    return;
+}
 
 /* end of file */
 
