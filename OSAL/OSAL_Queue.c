f2a46455a1220fa6f82b696e7dd26d11ed376121
Ian Hom <ianhom@163.com>
2016-06-11 10:49:03 +0800

    Update OSAL_Queue.c

--- OSAL/OSAL_Queue.c
+++ OSAL/OSAL_Queue.c
@@ -16,9 +16,6 @@
 #include "OSAL_Queue.h"
 #include "debug.h"
 
-static uint8  sg_u8No = 0;
-
-
 /******************************************************************************
 * Name       : uint8 Osal_Queue_Create(uint8 u8Len, uint8 u8Num)
 * Function   : Create a queue and return the queue number
@@ -47,14 +44,8 @@ T_QUEUE_INFO* Osal_Queue_Create(uint8 u8Len, uint8 u8Cnt)
     ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
     /**************************************************************************************************/
     ptQueueInfo->pu8Addr = (uint8*)OSAL_MALLOC(u16Size);
-    ptQueueInfo->u8No    = sg_u8No++;
-
-    /* If the Queue number is bigger than 255 */
-    if(0xFF == sg_u8No)
-    {
-        DBG_PRINT("Too many queue are created!!\n")
-        return NULL;
-    }
+    ptQueueInfo->u8Begin = 0;
+    ptQueueInfo->u8End   = 0;
     /**************************************************************************************************/
     EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */    
 
@@ -62,14 +53,92 @@ T_QUEUE_INFO* Osal_Queue_Create(uint8 u8Len, uint8 u8Cnt)
 
 }
 
-void Osal_Queue_Inc(T_QUEUE_INFO *ptQueue)
+uint8 Osal_Queue_Inc(T_QUEUE_INFO *ptQueue)
 {
+    uint32        u32IntSt;
     /* If the pointer of queue info is invalid */
     if(NULL == ptQueue)
     {
-        
+        DBG_PRINT("Invalid pointer of queue informtion!!\n");
+        return SW_ERR;
     }
+
+    if(SW_ERR == Osal_Queue_Is_Free(ptQueue))
+    {
+        return SW_ERR;
+    }
+
+    ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
+    /**************************************************************************************************/
+    ptQueue->u8End = (ptQueue->u8End + 1) % ptQueue->u8MaxCnt;
+    ptQueue->u8Cnt++;
+    /**************************************************************************************************/
+    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */    
+
+    
 }
+
+uint8 Osal_Queue_Dec(T_QUEUE_INFO *ptQueue)
+{
+    uint32        u32IntSt;
+    /* If the pointer of queue info is invalid */
+    if(NULL == ptQueue)
+    {
+        DBG_PRINT("Invalid pointer of queue informtion!!\n");
+        return SW_ERR;
+    }
+
+    if(SW_OK == Osal_Queue_Is_Empty(ptQueue))
+    {
+        return SW_ERR;
+    }
+
+    ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
+    /**************************************************************************************************/
+    ptQueue->u8Begin = (ptQueue->u8Begin + 1) % ptQueue->u8MaxCnt;
+    ptQueue->u8Cnt--;
+    /**************************************************************************************************/
+    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */   
+}
+
+uint8 Osal_Queue_Is_Free(T_QUEUE_INFO *ptQueue)
+{
+    uint32        u32IntSt;
+    /* If the pointer of queue info is invalid */
+    if(NULL == ptQueue)
+    {
+        DBG_PRINT("Invalid pointer of queue informtion!!\n");
+        return SW_ERR;
+    }
+
+    if(ptQueue->u8Cnt >= ptQueue->u8MaxCnt)
+    {
+        DBG_PRINT("The queue is full!!\n");
+        return SW_ERR;
+    }
+    DBG_PRINT("The queue is NOT full!!\n");
+    return SW_OK;
+}
+
+uint8 Osal_Queue_Is_Empty(T_QUEUE_INFO *ptQueue)
+{
+    uint32        u32IntSt;
+    /* If the pointer of queue info is invalid */
+    if(NULL == ptQueue)
+    {
+        DBG_PRINT("Invalid pointer of queue informtion!!\n");
+        return SW_ERR;
+    }
+
+    if(0 != ptQueue->u8Cnt)
+    {
+        DBG_PRINT("The queue is NOT empty!!\n");
+        return SW_ERR;
+    }
+    DBG_PRINT("The queue is empty!!\n");
+    return SW_OK;
+}
+
 
 /******************************************************************************
 * Name       : uint8 Osal_Queue_Delete(T_QUEUE_INFO* ptQueueInfo)
