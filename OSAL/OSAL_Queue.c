fc0eff3eef11c0c822279c8b067505797c19096f
Ian Hom <ianhom@163.com>
2016-06-11 09:37:07 +0800

    Update OSAL_Queue.c

--- OSAL/OSAL_Queue.c
+++ OSAL/OSAL_Queue.c
@@ -16,6 +16,8 @@
 #include "OSAL_Queue.h"
 #include "debug.h"
 
+static uint8  sg_u8No = 0;
+
 
 /******************************************************************************
 * Name       : uint8 Osal_Queue_Create(uint8 u8Len, uint8 u8Num)
@@ -29,18 +31,81 @@
 * Author     : Ian
 * Date       : 2nd Jun 2016
 ******************************************************************************/
-uint8 Osal_Queue_Create(uint8 u8Len, uint8 u8Num)
+T_QUEUE_INFO* Osal_Queue_Create(uint8 u8Len, uint8 u8Cnt)
 {
-    uint32 u32IntSt;
+    uint32        u32IntSt;
+    uint16        u16Size     = u8Len * u8Cnt;
+    T_QUEUE_INFO *ptQueueInfo = NULL;
+
+    /* If the buffer size is 0, return NULL */
+    if(0 == u16Size)
+    {
+        DBG_PRINT("The length of buffer is invalid!!\n");
+        return NULL;
+    }
 
     ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
     /**************************************************************************************************/
-    /**************************************************************************************************/
-    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
+    ptQueueInfo->pu8Addr = (uint8*)OSAL_MALLOC(u16Size);
+    ptQueueInfo->u8No    = sg_u8No++;
 
-    return e;
+    /* If the Queue number is bigger than 255 */
+    if(0xFF == sg_u8No)
+    {
+        DBG_PRINT("Too many queue are created!!\n")
+        return NULL;
+    }
+    /**************************************************************************************************/
+    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */    
+
+    return ptQueueInfo;
 
 }
+
+void Osal_Queue_Inc(T_QUEUE_INFO *ptQueue)
+{
+    /* If the pointer of queue info is invalid */
+    if(NULL == ptQueue)
+    {
+        
+    }
+}
+
+/******************************************************************************
+* Name       : uint8 Osal_Queue_Delete(T_QUEUE_INFO* ptQueueInfo)
+* Function   : Create a queue and return the queue number
+* Input      : PF_TIMER_SRC pfSysTm   Funtion which returns system time
+* Output:    : None
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 2nd Jun 2016
+******************************************************************************/
+uint8 Osal_Queue_Delete(T_QUEUE_INFO* ptQueueInfo)
+{
+    uint32        u32IntSt;
+    T_QUEUE_INFO *ptQueueInfo = NULL;
+
+    /* If the pointer of queue information is invalid */
+    if(NULL == ptQueueInfo)
+    {
+        DBG_PRINT("The length of buffer is invalid!!\n");
+        return SW_ERR;
+    }
+
+    ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
+    /**************************************************************************************************/
+    OSAL_FREE(ptQueueInfo->pu8Addr);
+    /**************************************************************************************************/
+    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */    
+
+    return SW_OK;
+
+
+}
+
 
 
 /* end of file */
