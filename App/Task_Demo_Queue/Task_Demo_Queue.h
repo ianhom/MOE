146d0826c4f4d4ce11ac178fd2d0f24648caf4f4
Ian Hom <ianhom@163.com>
2016-06-19 17:11:59 +0800

    Create Task_Demo_Queue.h

---
+++ App/Task_Demo_Queue/Task_Demo_Queue.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+* File       : Task_Demo_Queue.h
+* Function   : It is demo task for queue
+* description: This task creates two queues,one is located in task space, and 
+*              the other is created by Queue API. Task sends two messages with 
+*              the two queues and recieves the two messages from the queues.           
+* Version    : V1.00
+* Author     : Ian
+* Date       : 19th Jun 2016
+* History    :  No.  When           Who      What
+*               1    19/Jun/2016    Ian      V1.00: Create
+******************************************************************************/
+
+
+#ifndef _TASK_DEMO_QUEUE_H_
+#define _TASK_DEMO_QUEUE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+/* Check if specified option is set for debugging */
+#ifndef __DEBUG_MODE_APP_TASK__DEMO_QUEUE                    
+#define __DEBUG_MODE      __DEBUG_NONE                          /* Default: None debugging info            */
+#else
+#ifdef __DEBUG_MODE
+#undef __DEBUG_MODE
+#endif
+#define __DEBUG_MODE      __DEBUG_MODE_APP_TASK__DEMO_QUEUE     /* According the set from project_config.h */
+#endif
+
+void Task_Demo_Queue_Init(uint8 u8TaskID);
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _TASK_DEMO_QUEUE_H */
+
+/* End of file */
+
+
