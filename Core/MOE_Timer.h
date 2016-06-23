c67d1e51546d99f675906933f13488c1d976300d
Ian Hom <ianhom@163.com>
2016-06-23 13:08:09 +0800

    Update MOE_Timer.h

--- Core/MOE_Timer.h
+++ Core/MOE_Timer.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* File       : OSAL_Timer.h
+* File       : MOE_Timer.h
 * Function   : Provide timer services.
 * description: To be done.           
 * Version    : V1.00
@@ -9,21 +9,21 @@
 *               1    06/May/2016    Ian           Create
 ******************************************************************************/
 
-#ifndef _OSAL_TIMER_H_
-#define _OSAL_TIMER_H_
+#ifndef _MOE_TIMER_H_
+#define _MOE_TIMER_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* Check if specified option is set for debugging */
-#ifndef __DEBUG_MODE_OSAL_TIMER                    
+#ifndef __DEBUG_MODE_MOE_TIMER                    
 #define __DEBUG_MODE      __DEBUG_NONE                /* Default: None debugging info            */
 #else
 #ifdef __DEBUG_MODE
 #undef __DEBUG_MODE
 #endif
-#define __DEBUG_MODE      __DEBUG_MODE_OSAL_TIMER     /* According the set from project_config.h */
+#define __DEBUG_MODE      __DEBUG_MODE_MOE_TIMER     /* According the set from project_config.h */
 #endif
 
 typedef uint32 (*PF_TIMER_SRC)(void);
@@ -34,9 +34,9 @@ typedef uint32 (*PF_TIMER_CB)(void *pPara);
 * Description: Structure of timer.
 * Memebers   : PF_TIMER_CB  pfTmCallback;                       Callback when time is up 
 *              void        *pPara;                              Parameters for callback  
-*              uint32      u32TmStart;      0~0xFFFFFFFF        Start time point         
-*              uint32      u32TmOut;        0~0xFFFFFFFF        Timeout time             
-*              uint16      u16Cnt;          0xFFFF              Periodic
+*              uint32       u32TmStart;     0~0xFFFFFFFF        Start time point         
+*              uint32       u32TmOut;       0~0xFFFFFFFF        Timeout time             
+*              uint16       u16Cnt;         0xFFFF              Periodic
 *                                           0~0xFFFE            Counts to start the timer  
 *******************************************************************************/
 typedef struct _T_TIMER
@@ -60,12 +60,12 @@ typedef struct _T_TIMER_NODE
 }T_TIMER_NODE;
 
 /* MACRO */
-#define OSAL_TMR_INFINITE_CNT   (0xFFFF)     /* Infinite restart count  */
+#define MOE_TMR_INFINITE_CNT   (0xFFFF)     /* Infinite restart count  */
 
 
 /******************************************************************************
-* Name       : uint8 Osal_Timer_Init(PF_TIMER_SRC pfSysTm)
-* Function   : Init OSAL timer
+* Name       : uint8 Moe_Timer_Init(PF_TIMER_SRC pfSysTm)
+* Function   : Init MOE timer
 * Input      : PF_TIMER_SRC pfSysTm   Funtion which returns system time
 * Output:    : None
 * Return     : SW_OK   Successful.
@@ -75,10 +75,10 @@ typedef struct _T_TIMER_NODE
 * Author     : Ian
 * Date       : 6th May 2016
 ******************************************************************************/
-uint8 Osal_Timer_Init(PF_TIMER_SRC pfSysTm);
+uint8 Moe_Timer_Init(PF_TIMER_SRC pfSysTm);
 
 /******************************************************************************
-* Name       : T_TIMER_NODE* Osal_Timer_Start(uint8 u8TaskID, uint16 u16Evt, uint16 u16Cnt, uint32 u32TmOut)
+* Name       : T_TIMER_NODE* Moe_Timer_Start(uint8 u8TaskID, uint16 u16Evt, uint16 u16Cnt, uint32 u32TmOut)
 * Function   : Start a timer
 * Input      : T_TIMER *ptTm     Pointer of timers set by user.
 * Output:    : None
@@ -89,10 +89,25 @@ uint8 Osal_Timer_Init(PF_TIMER_SRC pfSysTm);
 * Author     : Ian
 * Date       : 6th May 2016
 ******************************************************************************/
-T_TIMER_NODE* Osal_Timer_Start(T_TIMER *ptTm);
+
+T_TIMER_NODE* Moe_Timer_Start(T_TIMER *ptTm);
 
 /******************************************************************************
-* Name       : T_TIMER_NODE* Osal_Timer_Stop(T_TIMER_NODE* ptNode)
+* Name       : T_TIMER_NODE* Moe_Timer_Delay(uint32 u32TmOut)
+* Function   : Start a timer for current task
+* Input      : T_TIMER *ptTm     Pointer of timers set by user.
+* Output:    : None
+* Return     : NULL           Fail to start a timer.
+*              T_TIMER_NODE*  The pointer of the timer which is started.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 23rd Jun 2016
+******************************************************************************/
+T_TIMER_NODE* Moe_Timer_Delay(uint32 u32TmOut);
+
+/******************************************************************************
+* Name       : T_TIMER_NODE* Moe_Timer_Stop(T_TIMER_NODE* ptNode)
 * Function   : Stop a started timer.
 * Input      : T_TIMER_NODE* ptNode  The pointer of node to be stopped
 * Output:    : None
@@ -103,13 +118,13 @@ T_TIMER_NODE* Osal_Timer_Start(T_TIMER *ptTm);
 * Author     : Ian
 * Date       : 6th May 2016
 ******************************************************************************/
-T_TIMER_NODE* Osal_Timer_Stop(T_TIMER_NODE* ptNode);
+T_TIMER_NODE* Moe_Timer_Stop(T_TIMER_NODE* ptNode);
 
 
 
 
 /******************************************************************************
-* Name       : static T_TIMER_NODE *Osal_Timer_Find(T_TIMER_NODE* ptNode)
+* Name       : static T_TIMER_NODE *Moe_Timer_Find(T_TIMER_NODE* ptNode)
 * Function   : Try to find a node.
 * Input      : T_TIMER_NODE* ptNode  The pointer of node to be found
 * Output:    : None
@@ -120,11 +135,11 @@ T_TIMER_NODE* Osal_Timer_Stop(T_TIMER_NODE* ptNode);
 * Author     : Ian
 * Date       : 6th May 2016
 ******************************************************************************/
-T_TIMER_NODE* Osal_Timer_Restart(T_TIMER_NODE* ptNode);
+T_TIMER_NODE* Moe_Timer_Restart(T_TIMER_NODE* ptNode);
 
 
 /******************************************************************************
-* Name       : uint8 Osal_Timer_Process(void)
+* Name       : uint8 Moe_Timer_Process(void)
 * Function   : Main process for timer updating.
 * Input      : None
 * Output:    : None
@@ -135,10 +150,10 @@ T_TIMER_NODE* Osal_Timer_Restart(T_TIMER_NODE* ptNode);
 * Author     : Ian
 * Date       : 6th May 2016
 ******************************************************************************/
-uint8 Osal_Timer_Process(void);
+uint8 Moe_Timer_Process(void);
 
 /******************************************************************************
-* Name       : uint16 Osal_Timer_Test_Cnt()
+* Name       : uint16 Moe_Timer_Test_Cnt()
 * Function   : Get the count of timers
 * Input      : None
 * Output:    : None
@@ -148,10 +163,10 @@ uint8 Osal_Timer_Process(void);
 * Author     : Ian
 * Date       : 6th May 2016
 ******************************************************************************/
-uint16 Osal_Timer_Test_Cnt();
+uint16 Moe_Timer_Test_Cnt();
 
 /******************************************************************************
-* Name       : void Osal_Timer_Test_General()
+* Name       : void Moe_Timer_Test_General()
 * Function   : General test
 * Input      : None
 * Output:    : None
@@ -162,7 +177,7 @@ uint16 Osal_Timer_Test_Cnt();
 * Author     : Ian
 * Date       : 9th May 2016
 ******************************************************************************/
-void Osal_Timer_Test_General();
+void Moe_Timer_Test_General();
 
 
  
@@ -170,7 +185,7 @@ void Osal_Timer_Test_General();
 }
 #endif
 
-#endif /* _OSAL_TIMER_H_ */
+#endif /* _MOE_TIMER_H_ */
 
 /* End of file */
 
