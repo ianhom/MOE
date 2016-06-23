ed11eb93baf7f3189bb1f2cf09c3b5c84178d81b
Ian Hom <ianhom@163.com>
2016-06-23 23:20:45 +0800

    Update MOE_Queue.c

--- Core/MOE_Queue.c
+++ Core/MOE_Queue.c
@@ -1,24 +1,24 @@
 /******************************************************************************
-* File       : OSAL_Queue.c
+* File       : MOE_Queue.c
 * Function   : General queue function.
 * description: This module realize common queue operations, You can create your 
-*              own queue in task space or use  Osal_Queue_Create() create a queue
+*              own queue in task space or use  Moe_Queue_Create() create a queue
 *              with malloc. Both method need a queue information data structure
 *              to record basic information used for queue operation. 
 *
 *              There are two method to use the queue:
-*              1。Use the simply API: Osal_Queue_Write() to fill the prepared
+*              1。Use the simply API: Moe_Queue_Write() to fill the prepared
 *                 data into the queue without checking free buffer block and queue
-*                 increase operation; Osal_Queue_Read() to read the data from the
+*                 increase operation; Moe_Queue_Read() to read the data from the
 *                 queue into the prepared array without check 0-buffer-block and 
 *                 decrease operation.
 *              2. For more efficient use, You can fill the buffer block by yourself
-*                 with the MACRO OSAL_QUEUE_LAST_FREE(p), or read data from queue 
-*                 with the MACRO OSAL_QUEUE_FIRST_USED(p), please remember to 
+*                 with the MACRO MOE_QUEUE_LAST_FREE(p), or read data from queue 
+*                 with the MACRO MOE_QUEUE_FIRST_USED(p), please remember to 
 *                 check queue is free/queue is NOT empty BEFORE wirting/reading 
-*                 by calling Osal_Qeueu_Is_Free()/Osal_Queue_Is_NOT_Empty(); And
+*                 by calling Moe_Qeueu_Is_Free()/Moe_Queue_Is_NOT_Empty(); And
 *                 increase/decrease the queue AFTER buffer block wirting/reading
-*                 by calling Osal_Queue_Inc()/Osal_Queue_Dec.
+*                 by calling Moe_Queue_Inc()/Moe_Queue_Dec.
 *
 *              Besides, this module provide a general test function which can be
 *              used for testing.      
@@ -32,12 +32,12 @@
 #include "type_def.h"
 #include "common_head.h"
 #include "project_config.h"
-#include "OSAL.h"
-#include "OSAL_Queue.h"
+#include "MOE_Core.h"
+#include "MOE_Queue.h"
 #include "debug.h"
 
 /******************************************************************************
-* Name       : uint8 Osal_Queue_Create(T_QUEUE_INFO *ptQueueInfo, uint8 u8Len, uint8 u8Cnt)
+* Name       : uint8 Moe_Queue_Create(T_QUEUE_INFO *ptQueueInfo, uint8 u8Len, uint8 u8Cnt)
 * Function   : Create a queue
 * Input      : uint8         u8Len          The length of each block in the queue
 *              uint8         u8Cnt          The count of blocks in th queue
@@ -49,12 +49,12 @@
 *              in your task space with T_QUEUE_INFO type, both way can use queue
 *              operation functions: write/read/increase/deccrease.
 *              If the queue which created by this function is unnecessary, please
-*              Call Osal_Queue_Delete() to delete it.
+*              Call Moe_Queue_Delete() to delete it.
 * Version    : V1.00
 * Author     : Ian
 * Date       : 10th Jun 2016
 ******************************************************************************/
-uint8 Osal_Queue_Create(T_QUEUE_INFO *ptQueueInfo, uint8 u8Len, uint8 u8Cnt)
+uint8 Moe_Queue_Create(T_QUEUE_INFO *ptQueueInfo, uint8 u8Len, uint8 u8Cnt)
 {
     uint32 u32IntSt;
     uint16 u16Size = u8Len * u8Cnt;
@@ -68,7 +68,7 @@ uint8 Osal_Queue_Create(T_QUEUE_INFO *ptQueueInfo, uint8 u8Len, uint8 u8Cnt)
 
     ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
     /**************************************************************************************************/
-    ptQueueInfo->pu8Addr  = (uint8*)OSAL_MALLOC(u16Size);  /* Create data space  */
+    ptQueueInfo->pu8Addr  = (uint8*)MOE_MALLOC(u16Size);   /* Create data space  */
     ptQueueInfo->u8Begin  = 0;                             /* Init begin pointer */
     ptQueueInfo->u8End    = 0;                             /* Init end pointer   */
     ptQueueInfo->u8Cnt    = 0;                             /* Init block count   */
@@ -83,20 +83,20 @@ uint8 Osal_Queue_Create(T_QUEUE_INFO *ptQueueInfo, uint8 u8Len, uint8 u8Cnt)
 }
 
 /******************************************************************************
-* Name       : uint8 Osal_Queue_Delete(T_QUEUE_INFO *ptQueueInfo)
+* Name       : uint8 Moe_Queue_Delete(T_QUEUE_INFO *ptQueueInfo)
 * Function   : Delete a queue
 * Input      : T_QUEUE_INFO *ptQueueInfo    The pointer of queue information data structure
 * Output:    : None.
 * Return     : SW_OK   Successful.
 *              SW_ERR  Failed.
 * description: This function is used to free the queue which is created by 
-*              Osal_Queue_Create(), NOTE: Please do NOT call this function to 
+*              Moe_Queue_Create(), NOTE: Please do NOT call this function to 
 *              free user queue located in task space (task static RAM usually).
 * Version    : V1.00
 * Author     : Ian
 * Date       : 10th Jun 2016
 ******************************************************************************/
-uint8 Osal_Queue_Delete(T_QUEUE_INFO *ptQueueInfo)
+uint8 Moe_Queue_Delete(T_QUEUE_INFO *ptQueueInfo)
 {
     uint32 u32IntSt;
 
@@ -109,7 +109,7 @@ uint8 Osal_Queue_Delete(T_QUEUE_INFO *ptQueueInfo)
 
     ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
     /**************************************************************************************************/
-    OSAL_FREE(ptQueueInfo->pu8Addr);
+    MOE_FREE(ptQueueInfo->pu8Addr);
     /**************************************************************************************************/
     EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */    
 
@@ -118,7 +118,7 @@ uint8 Osal_Queue_Delete(T_QUEUE_INFO *ptQueueInfo)
 
 
 /******************************************************************************
-* Name       : uint8 Osal_Queue_Inc(T_QUEUE_INFO *ptQueue)
+* Name       : uint8 Moe_Queue_Inc(T_QUEUE_INFO *ptQueue)
 * Function   : Update the end pointer of used block.
 * Input      : T_QUEUE_INFO *ptQueueInfo    The pointer of queue information data structure
 * Output:    : None
@@ -126,13 +126,13 @@ uint8 Osal_Queue_Delete(T_QUEUE_INFO *ptQueueInfo)
 *              SW_ERR  Failed.
 * description: This function increases the end buffer pointer of the queue, please
 *              call this function after free buffer checking and wirting operation.
-*              NOTE: Do NOT call such function if  "Osal_Queue_Write()" function is
+*              NOTE: Do NOT call such function if  "Moe_Queue_Write()" function is
 *              called.
 * Version    : V1.00
 * Author     : Ian
 * Date       : 11th Jun 2016
 ******************************************************************************/
-uint8 Osal_Queue_Inc(T_QUEUE_INFO *ptQueue)
+uint8 Moe_Queue_Inc(T_QUEUE_INFO *ptQueue)
 {
     uint32 u32IntSt;
     /* If the pointer of queue info is invalid */
@@ -160,7 +160,7 @@ uint8 Osal_Queue_Inc(T_QUEUE_INFO *ptQueue)
 }
 
 /******************************************************************************
-* Name       : uint8 Osal_Queue_Inc(T_QUEUE_INFO *ptQueue)
+* Name       : uint8 Moe_Queue_Inc(T_QUEUE_INFO *ptQueue)
 * Function   : Update the begin pointer of used block.
 * Input      : T_QUEUE_INFO *ptQueueInfo    The pointer of queue information data structure
 * Output:    : None
@@ -168,13 +168,13 @@ uint8 Osal_Queue_Inc(T_QUEUE_INFO *ptQueue)
 *              SW_ERR  Failed.
 * description: This function increases the start buffer pointer of the queue, please
 *              call this function after empty buffer checking and reading operation.
-*              NOTE: Do NOT call such function if  "Osal_Queue_Read()" function is
+*              NOTE: Do NOT call such function if  "Moe_Queue_Read()" function is
 *              called.
 * Version    : V1.00
 * Author     : Ian
 * Date       : 11th Jun 2016
 ******************************************************************************/
-uint8 Osal_Queue_Dec(T_QUEUE_INFO *ptQueue)
+uint8 Moe_Queue_Dec(T_QUEUE_INFO *ptQueue)
 {
     uint32 u32IntSt;
     /* If the pointer of queue info is invalid */
@@ -202,7 +202,7 @@ uint8 Osal_Queue_Dec(T_QUEUE_INFO *ptQueue)
 }
 
 /******************************************************************************
-* Name       : uint8 Osal_Queue_Is_Free(T_QUEUE_INFO *ptQueue)
+* Name       : uint8 Moe_Queue_Is_Free(T_QUEUE_INFO *ptQueue)
 * Function   : Check if the queue is free
 * Input      : T_QUEUE_INFO* ptQueueInfo    The pointer of queue information data structure
 * Output:    : None
@@ -210,12 +210,12 @@ uint8 Osal_Queue_Dec(T_QUEUE_INFO *ptQueue)
 *              SW_ERR  Not Free.
 * description: This function checks if queue is free or NOT by used buffer blocks.
 *              Please call this function before queue writing except calling API 
-*              "Osal_Queue_Write()".
+*              "Moe_Queue_Write()".
 * Version    : V1.00
 * Author     : Ian
 * Date       : 11th Jun 2016
 ******************************************************************************/
-uint8 Osal_Queue_Is_Free(T_QUEUE_INFO *ptQueue)
+uint8 Moe_Queue_Is_Free(T_QUEUE_INFO *ptQueue)
 {
     /* If the pointer of queue info is invalid */
     if(NULL == ptQueue)
@@ -235,7 +235,7 @@ uint8 Osal_Queue_Is_Free(T_QUEUE_INFO *ptQueue)
 }
 
 /******************************************************************************
-* Name       : uint8 Osal_Queue_Is_Not_Empty(T_QUEUE_INFO *ptQueue)
+* Name       : uint8 Moe_Queue_Is_Not_Empty(T_QUEUE_INFO *ptQueue)
 * Function   : Check if the queue is NOT empty
 * Input      : T_QUEUE_INFO *ptQueueInfo    The pointer of queue information data structure
 * Output:    : None
@@ -243,12 +243,12 @@ uint8 Osal_Queue_Is_Free(T_QUEUE_INFO *ptQueue)
 *              SW_ERR  Empty.
 * description: This function checks if queue is Not empty or NOT by used buffer 
 *              blocks. Please call this function before queue writing except 
-*              calling API "Osal_Queue_Read()".
+*              calling API "Moe_Queue_Read()".
 * Version    : V1.00
 * Author     : Ian
 * Date       : 11th Jun 2016
 ******************************************************************************/
-uint8 Osal_Queue_Is_Not_Empty(T_QUEUE_INFO *ptQueue)
+uint8 Moe_Queue_Is_Not_Empty(T_QUEUE_INFO *ptQueue)
 {
     /* If the pointer of queue info is invalid */
     if(NULL == ptQueue)
@@ -268,7 +268,7 @@ uint8 Osal_Queue_Is_Not_Empty(T_QUEUE_INFO *ptQueue)
 }
 
 /******************************************************************************
-* Name       : uint8 Osal_Queue_Write(T_QUEUE_INFO *ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
+* Name       : uint8 Moe_Queue_Write(T_QUEUE_INFO *ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
 * Function   : Write into the queue with the desired data
 * Input      : T_QUEUE_INFO *ptQueueInfo    The pointer of queue information data structure
 *              uint8        *pu8Data        The pointer of data to be writen
@@ -278,13 +278,13 @@ uint8 Osal_Queue_Is_Not_Empty(T_QUEUE_INFO *ptQueue)
 *              SW_ERR  Failed.
 * description: This function checks if the queue is free or NOT before data writing,
 *              and increases the queue after data writing inside.
-*              NOTE: When this function is called, Osal_Queue_Inc() should NOT be
+*              NOTE: When this function is called, Moe_Queue_Inc() should NOT be
 *              called again because it will be done inside of such function.
 * Version    : V1.00
 * Author     : Ian
 * Date       : 11th Jun 2016
 ******************************************************************************/
-uint8 Osal_Queue_Write(T_QUEUE_INFO *ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
+uint8 Moe_Queue_Write(T_QUEUE_INFO *ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
 {
     uint32 u32IntSt;
 
@@ -313,7 +313,7 @@ uint8 Osal_Queue_Write(T_QUEUE_INFO *ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
     /**************************************************************************************************/
     for(uint8 u8Idx = 0; u8Idx < u8Len; u8Idx++)
     {
-        OSAL_QUEUE_LAST_FREE(ptQueueInfo)[u8Idx] = pu8Data[u8Idx];
+        MOE_QUEUE_LAST_FREE(ptQueueInfo)[u8Idx] = pu8Data[u8Idx];
     }
     ptQueueInfo->u8End = (ptQueueInfo->u8End + 1) % ptQueueInfo->u8MaxCnt;  /* Update end pointer */
     ptQueueInfo->u8Cnt++;                                                   /* Update used count  */
@@ -325,7 +325,7 @@ uint8 Osal_Queue_Write(T_QUEUE_INFO *ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
 }
 
 /******************************************************************************
-* Name       : uint8 Osal_Queue_Write(T_QUEUE_INFO *ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
+* Name       : uint8 Moe_Queue_Write(T_QUEUE_INFO *ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
 * Function   : Write into the queue with the desired data
 * Input      : T_QUEUE_INFO *ptQueueInfo    The pointer of queue information data structure
 *              uint8        *pu8Data        The pointer of data to be writen
@@ -335,13 +335,13 @@ uint8 Osal_Queue_Write(T_QUEUE_INFO *ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
 *              SW_ERR  Failed.
 * description: This function checks if the queue is not empty or NOT before data
 *              reading, and decreases the queue after data reading inside.
-*              NOTE: When this function is called, Osal_Queue_Dec() should NOT be
+*              NOTE: When this function is called, Moe_Queue_Dec() should NOT be
 *              called again because it will be done inside of such function.
 * Version    : V1.00
 * Author     : Ian
 * Date       : 11th Jun 2016
 ******************************************************************************/
-uint8 Osal_Queue_Read(T_QUEUE_INFO* ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
+uint8 Moe_Queue_Read(T_QUEUE_INFO* ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
 {
     uint32 u32IntSt;
 
@@ -371,14 +371,14 @@ uint8 Osal_Queue_Read(T_QUEUE_INFO* ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
     /**************************************************************************************************/
     for(uint8 u8Idx = 0; u8Idx < u8Len; u8Idx++)
     {
-         pu8Data[u8Idx] = OSAL_QUEUE_LAST_FREE(ptQueueInfo)[u8Idx];
+        pu8Data[u8Idx] = MOE_QUEUE_LAST_FREE(ptQueueInfo)[u8Idx];
     }
     ptQueueInfo->u8Begin = (ptQueueInfo->u8Begin + 1) % ptQueueInfo->u8MaxCnt;  /* Update the begin pointer */
     ptQueueInfo->u8Cnt--;                                               /* Update the used count    */
     /**************************************************************************************************/
     EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */    
 
-    Osal_Queue_Dec(ptQueueInfo); 
+    Moe_Queue_Dec(ptQueueInfo); 
 
     return SW_OK;
     
@@ -386,7 +386,7 @@ uint8 Osal_Queue_Read(T_QUEUE_INFO* ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
 
 
 /******************************************************************************
-* Name       : void Osal_Queue_Test_General()
+* Name       : void Moe_Queue_Test_General()
 * Function   : General test for queue
 * Input      : None
 * Output:    : None
@@ -397,25 +397,25 @@ uint8 Osal_Queue_Read(T_QUEUE_INFO* ptQueueInfo, uint8 *pu8Data, uint8 u8Len)
 * Author     : Ian
 * Date       : 11th Jun 2016
 ******************************************************************************/
-void Osal_Queue_Test_General()
+void Moe_Queue_Test_General()
 {
     static T_QUEUE_INFO sg_tQueue;
     static uint8 sg_au8DataW[4] = {4,3,2,1};
     static uint8 sg_au8DataR[4] = {0};
     
-    Osal_Queue_Create(&sg_tQueue,4,5);
+    Moe_Queue_Create(&sg_tQueue,4,5);
     DBG_PRINT("A queue with 4-byte-length, 5-blocks is created!!\n ");
 
     /* Fill up the queue with test data */
-    while(SW_OK == Osal_Queue_Is_Free(&sg_tQueue))
+    while(SW_OK == Moe_Queue_Is_Free(&sg_tQueue))
     {
-        Osal_Queue_Write(&sg_tQueue, sg_au8DataW,sizeof(sg_au8DataW));
+        Moe_Queue_Write(&sg_tQueue, sg_au8DataW,sizeof(sg_au8DataW));
     }
     
     /* Empty the queue */
-    while(SW_OK == Osal_Queue_Is_Not_Empty(&sg_tQueue))
+    while(SW_OK == Moe_Queue_Is_Not_Empty(&sg_tQueue))
     {
-        Osal_Queue_Read(&sg_tQueue, sg_au8DataR,sizeof(sg_au8DataR));
+        Moe_Queue_Read(&sg_tQueue, sg_au8DataR,sizeof(sg_au8DataR));
     }    
     
     return;
