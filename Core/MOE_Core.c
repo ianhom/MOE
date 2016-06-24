5dc9f8057e07f4c3aba79ad2eed9413bdf8e3d7c
Ian Hom <ianhom@163.com>
2016-06-24 11:16:41 +0800

    Update MOE_Core.c

--- Core/MOE_Core.c
+++ Core/MOE_Core.c
@@ -1,6 +1,6 @@
 /******************************************************************************
-* File       : OSAL.c
-* Function   : Provide the main function of OSAL-like scheduler.
+* File       : MOE.c
+* Function   : Provide the main function of MOE-like scheduler.
 * description: To be done.          
 * Version    : V1.00
 * Author     : Ian
@@ -12,11 +12,12 @@
 #include "type_def.h"
 #include "common_head.h"
 #include "project_config.h"
-#include "OSAL.h"
+#include "MOE_Core.h"
 #include "debug.h"
-#include "OSAL_App.h"
-#include "OSAL_Timer.h"
-#include "OSAL_Msg.h"
+#include "MOE_App.h"
+#include "MOE_Timer.h"
+#include "MOE_Msg.h"
+#include "MOE_Event.h"
 
 
 static PF_MALLOC sg_pfMalloc = NULL;
@@ -29,20 +30,23 @@ static PF_TASK_PROCESS sg_apfTaskFn[MAX_TASK_NUM];      /* Create a list of proc
 
 uint16 au16TaskEvt[MAX_TASK_NUM];                       /* Create a event list for tasks                  */
 
+T_EVENT sg_tEvt;
+
+
 /******************************************************************************
-* Name       : void Osal_Reg_Tasks(PF_TASK_PROCESS pfTaskFn)
+* Name       : void Moe_Reg_Tasks(PF_TASK_PROCESS pfTaskFn)
 * Function   : Register task process function into the table.
 * Input      : PF_TASK_PROCESS pfTaskFn    Task process function pointer
 * Output:    : None
 * Return     : None
 * description: 1. Check if the input function pointer is invalid.
-*              2. Check if the registered task number is invlide.
+*              2. Check if the registered task number is invalid.
 *              3. Store the function pointer into the table.
 * Version    : V1.00
 * Author     : Ian
 * Date       : 3rd May 2016
 ******************************************************************************/
-void Osal_Reg_Tasks(PF_TASK_PROCESS pfTaskFn)
+void Moe_Reg_Tasks(PF_TASK_PROCESS pfTaskFn)
 {
     static uint8 s_u8RegTaskCnt = 0;  /* Start the index of task process function array */
 
@@ -66,7 +70,7 @@ void Osal_Reg_Tasks(PF_TASK_PROCESS pfTaskFn)
 }
 
 /******************************************************************************
-* Name       : void Osal_Memset(uint8* pDes, uint8 u8Val, uint8 u8Len)
+* Name       : void Moe_Memset(uint8* pDes, uint8 u8Val, uint8 u8Len)
 * Function   : Set a memory block with a desired value
 * Input      : uint8* pDes   The destination pointer
 *              uint8 u8Val   The desired set to be set
@@ -79,7 +83,7 @@ void Osal_Reg_Tasks(PF_TASK_PROCESS pfTaskFn)
 * Author     : Ian
 * Date       : 3rd May 2016
 ******************************************************************************/
-uint8 Osal_Memset(uint8* pDes, uint8 u8Val, uint8 u8Len)
+uint8 Moe_Memset(uint8* pDes, uint8 u8Val, uint8 u8Len)
 {   
     uint8 u8Idx;
     /* Check if the pointer is invalid or NOT */
@@ -96,8 +100,11 @@ uint8 Osal_Memset(uint8* pDes, uint8 u8Val, uint8 u8Len)
     return SW_OK;
 }
 
+
+
+
 /******************************************************************************
-* Name       : uint8 Osal_Event_Set(uint8 u8TaskID, uint16 Event)
+* Name       : uint8 Moe_Event_Set(uint8 u8TaskID, uint16 Event)
 * Function   : To be done
 * Input      : uint8  u8TaskID
 *              uint16 u16Evt
@@ -109,54 +116,7 @@ uint8 Osal_Memset(uint8* pDes, uint8 u8Val, uint8 u8Len)
 * Author     : Ian
 * Date       : 3rd May 2016
 ******************************************************************************/
-uint8 Osal_Event_Set(uint8 u8TaskID, uint16 u16Evt)
-{  
-    uint8  u8Idx;
-    uint32 u32IntSt;
-
-    if(TASK_ALL_TASK == u8TaskID)   /* If it is an event for all tasks */
-    {
-        DBG_PRINT("It is an event for all tasks\n");
-        ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
-        /**************************************************************************************************/
-        for(u8Idx = 0; u8Idx < MAX_TASK_NUM; u8Idx++)
-        {
-            au16TaskEvt[u8Idx] |= u16Evt;              /* Set all tasks with the event */
-        }
-        /**************************************************************************************************/
-        EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
-        return SW_OK;
-    }
-
-    /* Check if the task ID is invalid or NOT */
-    if(u8TaskID > MAX_TASK_NUM)
-    {   /* If task ID is wrong, return error */
-        return SW_ERR;
-    }
-
-    ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
-    /**************************************************************************************************/
-    au16TaskEvt[u8TaskID - 1] |= u16Evt;
-    /**************************************************************************************************/
-    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
-
-    return SW_OK;
-}
-
-/******************************************************************************
-* Name       : uint8 Osal_Event_Set(uint8 u8TaskID, uint16 Event)
-* Function   : To be done
-* Input      : uint8  u8TaskID
-*              uint16 u16Evt
-* Output:    : None
-* Return     : SW_OK   Successful.
-*              SW_ERR  Failed.
-* description: To be done
-* Version    : V1.00
-* Author     : Ian
-* Date       : 3rd May 2016
-******************************************************************************/
-uint8 Osal_Event_Clr(uint8 u8TaskID, uint16 u16Evt)
+uint8 Moe_Event_Clr(uint8 u8TaskID, uint16 u16Evt)
 {  
     uint8  u8Idx;
     uint32 u32IntSt;
@@ -192,7 +152,7 @@ uint8 Osal_Event_Clr(uint8 u8TaskID, uint16 u16Evt)
 
 
 /******************************************************************************
-* Name       : void Osal_Init()
+* Name       : void Moe_Init()
 * Function   : Init all tasks
 * Input      : None
 * Output:    : None
@@ -205,18 +165,24 @@ uint8 Osal_Event_Clr(uint8 u8TaskID, uint16 u16Evt)
 * Author     : Ian
 * Date       : 29th Apr 2016
 ******************************************************************************/
-void Osal_Init()
+void Moe_Init()
 {
     uint32 u8Idx;    
-
+   
+    sg_tEvt.u8Task = TASK_NO_TASK;
+    sg_tEvt.u8Evt  = EVENT_NONE;
+    
     /* Init the task events list with NO EVENT                */
-    Osal_Memset((uint8*)au16TaskEvt, EVENT_NONE, sizeof(uint16)*MAX_TASK_NUM);
+    Moe_Memset((uint8*)au16TaskEvt, EVENT_NONE, sizeof(uint8)*MAX_TASK_NUM);
    
     /* Init the task process function pointer table with NULL */
-    Osal_Memset((uint8*)sg_apfTaskFn, NULL, sizeof(uint16*)*MAX_TASK_NUM);
+    Moe_Memset((uint8*)sg_apfTaskFn, NULL, sizeof(uint16*)*MAX_TASK_NUM);
 
+    /* Init event mechanism */
+    Moe_Event_Init();
+    
     /* Init all tasks */
-    Osal_Tasks_Init();
+    Moe_Tasks_Init();
 
     /* Check all task process function pointers are registered */
     for(u8Idx = 0; u8Idx < MAX_TASK_NUM; u8Idx++)
@@ -232,7 +198,7 @@ void Osal_Init()
 }
 
 /******************************************************************************
-* Name       : void Osal_ProcessPoll()
+* Name       : void Moe_ProcessPoll()
 * Function   : To be done.
 * Input      : None
 * Output:    : None
@@ -242,7 +208,7 @@ void Osal_Init()
 * Author     : Ian
 * Date       : 3rd May 2016
 ******************************************************************************/
-void Osal_ProcessPoll()
+void Moe_ProcessPoll()
 {
     /* To be done... */
     return;
@@ -250,7 +216,7 @@ void Osal_ProcessPoll()
 
 
 /******************************************************************************
-* Name       : void Osal_Run_System()
+* Name       : void Moe_Run_System()
 * Function   : The main function to schedule tasks.
 * Input      : None
 * Output:    : None
@@ -260,55 +226,26 @@ void Osal_ProcessPoll()
 * Author     : Ian
 * Date       : 28th Apr 2016
 ******************************************************************************/
-void Osal_Run_System()
+void Moe_Run_System()
 {
-    uint8 u8Idx = 0;
-    for(;;)                               /* The main loop                */
+    while(1)                             /* The main loop                */
     {
-        Osal_Timer_Process();             /* Update all timers            */
-        Osal_Msg_Process();               /* Process messageas            */
-        Osal_ProcessPoll();               /* Do polling process if needed */
-       
-        do
+        Moe_Timer_Process();             /* Update all timers            */
+        Moe_Msg_Process();               /* Process messageas            */
+        Moe_ProcessPoll();               /* Do polling process if needed */
+
+        if(Moe_Event_Get(&sg_tEvt))
         {
-            if(au16TaskEvt[u8Idx])        /* Check if events happen for such task */
-            {       
-                break;                     /* If so, break loop                    */
-            }
-            /* If there is NO events for such task, do nothing and continue next task checking */
-        }while(++u8Idx < MAX_TASK_NUM);   /* Check all tasks */
-        /* Reach here if an event happens for a task, or No event for all tasks   */
-       
-        if(u8Idx < MAX_TASK_NUM)          /* If an event happens for a task       */
-        {
-            uint16 u16Evt;
-            uint32 u32IntSt;
-           
-            ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
-            /**************************************************************************************************/
-            u16Evt = au16TaskEvt[u8Idx];    /* Get the event             */
-            au16TaskEvt[u8Idx] = 0;         /* Clean the event temporary */
-            /**************************************************************************************************/
-            EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
+            sg_apfTaskFn[sg_tEvt.u8Task - 1](sg_tEvt.u8Evt);    /* Call the task process function */
+            sg_tEvt.u8Task = TASK_NO_TASK;             /* Finish task processing and cancel active task mark */
             
-            sg_u8ActiveTask = u8Idx + 1;                   /* Save the active task number    */
-            u16Evt = (sg_apfTaskFn[u8Idx](u16Evt));        /* Call the task process function */
-            sg_u8ActiveTask = TASK_NO_TASK;                /* Finish task processing and cancel active task mark */
-           
-           ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
-           /**************************************************************************************************/
-            au16TaskEvt[u8Idx] |= u16Evt;  /* Add the rest events back */
-           /**************************************************************************************************/
-           EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
         }
-        /* If u8Idx is NOT lower than u8TaskCnt, it indicate that NO event happens and nothing need to be done */
-        u8Idx = 0;
     }
     return;
 }
 
 /******************************************************************************
-* Name       : uint8 Osal_Get_Acktive_Task()
+* Name       : uint8 Moe_Get_Acktive_Task()
 * Function   : To be done.
 * Input      : None
 * Output:    : None
@@ -318,13 +255,13 @@ void Osal_Run_System()
 * Author     : Ian
 * Date       : 3rd May 2016
 ******************************************************************************/
-uint8 Osal_Get_Acktive_Task()
+uint8 Moe_Get_Acktive_Task()
 {
-    return sg_u8ActiveTask;
+    return sg_tEvt.u8Task;
 }
 
 /******************************************************************************
-* Name       : void Osal_Reg_Malloc_Free(PF_MALLOC pfMalloc, PF_FREE pfFree)
+* Name       : void Moe_Reg_Malloc_Free(PF_MALLOC pfMalloc, PF_FREE pfFree)
 * Function   : To be done.
 * Input      : None
 * Output:    : None
@@ -334,7 +271,7 @@ uint8 Osal_Get_Acktive_Task()
 * Author     : Ian
 * Date       : 25th May 2016
 ******************************************************************************/
-void Osal_Reg_Malloc_Free(PF_MALLOC pfMalloc, PF_FREE pfFree)
+void Moe_Reg_Malloc_Free(PF_MALLOC pfMalloc, PF_FREE pfFree)
 {
     sg_pfMalloc = pfMalloc;  /* Get malloc function */
     sg_pfFree   = pfFree;    /* Get free function   */
@@ -342,7 +279,7 @@ void Osal_Reg_Malloc_Free(PF_MALLOC pfMalloc, PF_FREE pfFree)
 }
 
 /******************************************************************************
-* Name       : void* Osal_Malloc(uint32 u32Size)
+* Name       : void* Moe_Malloc(uint32 u32Size)
 * Function   : To be done.
 * Input      : None
 * Output:    : None
@@ -352,7 +289,7 @@ void Osal_Reg_Malloc_Free(PF_MALLOC pfMalloc, PF_FREE pfFree)
 * Author     : Ian
 * Date       : 25th May 2016
 ******************************************************************************/
-void* Osal_Malloc(uint32 u32Size)
+void* Moe_Malloc(uint32 u32Size)
 {   
     /* Check if malloc and free function is registered */
     if((NULL != sg_pfMalloc) && (NULL != sg_pfFree) )
@@ -365,7 +302,7 @@ void* Osal_Malloc(uint32 u32Size)
 }
 
 /******************************************************************************
-* Name       : void Osal_Free(void *p)
+* Name       : void Moe_Free(void *p)
 * Function   : To be done.
 * Input      : None
 * Output:    : None
@@ -375,7 +312,7 @@ void* Osal_Malloc(uint32 u32Size)
 * Author     : Ian
 * Date       : 25th May 2016
 ******************************************************************************/
-void Osal_Free(void *p)
+void Moe_Free(void *p)
 {
     /* Check if malloc and free function is registered */
     if((NULL != sg_pfMalloc) && (NULL != sg_pfFree) )
@@ -387,6 +324,42 @@ void Osal_Free(void *p)
     /* A malloc function to be done here later */
     return;    
 }
+
+
+uint8 Moe_Process_Task(uint8 u8Task)
+{   
+    uint8  u8Idx;
+    uint16 u16Evt;
+    uint32 u32IntSt;
+
+    if((TASK_NO_TASK == u8Task) ||(u8Task > MAX_TASK_NUM))
+    {
+        return SW_ERR;
+    }
+
+    u8Idx = u8Task - 1;
+
+
+    ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
+    /**************************************************************************************************/
+    u16Evt = au16TaskEvt[u8Idx];    /* Get the event             */
+    au16TaskEvt[u8Idx] = 0;         /* Clean the event temporary */
+    /**************************************************************************************************/
+    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
+    
+    sg_u8ActiveTask = u8Task;                   /* Save the active task number    */
+    u16Evt = (sg_apfTaskFn[u8Idx](u16Evt));     /* Call the task process function */
+    sg_u8ActiveTask = TASK_NO_TASK;             /* Finish task processing and cancel active task mark */
+    
+    ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
+    /**************************************************************************************************/
+    au16TaskEvt[u8Idx] |= u16Evt;   /* Add the rest events back */
+    /**************************************************************************************************/
+    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
+
+    return SW_OK;
+}
+
 
 
 /* End of file */
