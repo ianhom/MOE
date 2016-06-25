a998b15121cd0887eedfae36e7ff468c866857d6
Ian Hom <ianhom@163.com>
2016-06-25 09:36:18 +0800

    Update MOE_App.h

--- Core/MOE_App.h
+++ Core/MOE_App.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* File       : OSAL_App.h
+* File       : MOE_App.h
 * Function   : User application definitions.
 * description: To be done.           
 * Version    : V1.00
@@ -9,34 +9,34 @@
 *               1    3/May/2016     Ian           Create
 ******************************************************************************/
 
-#ifndef _OSAL_APP_H_
-#define _OSAL_APP_H_
+#ifndef _MOE_APP_H_
+#define _MOE_APP_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* Check if specified option is set for debugging */
-#ifndef __DEBUG_MODE_OSAL_APP                    
+#ifndef __DEBUG_MODE_MOE_APP                    
 #define __DEBUG_MODE      __DEBUG_NONE                /* Default: None debugging info            */
 #else
 #ifdef __DEBUG_MODE
 #undef __DEBUG_MODE
 #endif
-#define __DEBUG_MODE      __DEBUG_MODE_OSAL_APP       /* According the set from project_config.h */
+#define __DEBUG_MODE      __DEBUG_MODE_MOE_APP       /* According the set from project_config.h */
 #endif
 
 #ifndef MAX_TASK_NUM
-#define MAX_TASK_NUM             (3)      /* Max number of tasks */
+#define MAX_TASK_NUM             (3)                  /* Max number of tasks */
 #endif
 
-void Osal_Tasks_Init();
+void Moe_Tasks_Init();
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* OSAL_APP_H */
+#endif /* MOE_APP_H */
 
 /* End of file */
 
