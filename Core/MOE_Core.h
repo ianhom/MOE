7e885acd0cacfa016439d60811894e67101dcc69
Ian Hom <ianhom@163.com>
2016-06-26 12:13:08 +0800

    Update MOE_Core.h

--- Core/MOE_Core.h
+++ Core/MOE_Core.h
@@ -41,6 +41,12 @@ typedef void* (*PF_MALLOC)(uint32 u32Size);
 typedef void (*PF_FREE)(void *p);
 typedef void (*PF_POLL)(void);
 
+#ifndef PF_TIMER_SRC
+#define PF_TIMER_SRC PF_TIMER_SRC
+typedef uint32 (*PF_TIMER_SRC)(void);
+#endif
+
+
 /* MOE MACRO */
 #define TASK_NO_TASK                (0x00)                    /* Task number which means there is no task */
 #define TASK_ALL_TASK               (0xFF)                    /* Task number which means all tasks        */
@@ -55,6 +61,124 @@ typedef void (*PF_POLL)(void);
 #define EVENT_INT                   (0x0008)                  /* There is an interrupt event              */
 #define EVENT_DELAY                 (0x0010)
 
+    
+/******************************************************************************
+* Name       : void Moe_Reg_Tasks(PF_TASK_PROCESS pfTaskFn)
+* Function   : Register task process function into the table.
+* Input      : PF_TASK_PROCESS pfTaskFn    Task process function pointer
+* Output:    : None
+* Return     : None
+* description: 1. Check if the input function pointer is invalid.
+*              2. Check if the registered task number is invalid.
+*              3. Store the function pointer into the table.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd May 2016
+******************************************************************************/
+void Moe_Reg_Tasks(PF_TASK_PROCESS pfTaskFn);
+
+/******************************************************************************
+* Name       : void Moe_Memset(uint8* pDes, uint8 u8Val, uint8 u8Len)
+* Function   : Set a memory block with a desired value
+* Input      : uint8* pDes   The destination pointer
+*              uint8 u8Val   The desired set to be set
+*              uint8 u8Len   The length of memory block in byte
+* Output:    : None
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: Set a memory block with a desired value
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd May 2016
+******************************************************************************/
+uint8 Moe_Memset(uint8* pDes, uint8 u8Val, uint8 u8Len);
+
+
+/******************************************************************************
+* Name       : void Moe_Init()
+* Function   : Init all tasks
+* Input      : None
+* Output:    : None
+* Return     : None
+* description: 1. Clear tasks events list with NO EVENT.
+*              2. Clear task process function pointer table with NULL.
+*              3. Init all tasks and pass the task ID into the tasks.
+*              4. Check all tasks are registered.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 29th Apr 2016
+******************************************************************************/
+uint8 Moe_Init(PF_TIMER_SRC pfSysTm, PF_POLL pfPoll);
+
+
+/******************************************************************************
+* Name       : void Moe_Run_System()
+* Function   : The main function to schedule tasks.
+* Input      : None
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 28th Apr 2016
+******************************************************************************/
+void Moe_Run();
+
+/******************************************************************************
+* Name       : uint8 Moe_Get_Acktive_Task()
+* Function   : To be done.
+* Input      : None
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd May 2016
+******************************************************************************/
+uint8 Moe_Get_Acktive_Task();
+
+/******************************************************************************
+* Name       : void Moe_Reg_Malloc_Free(PF_MALLOC pfMalloc, PF_FREE pfFree)
+* Function   : To be done.
+* Input      : None
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 25th May 2016
+******************************************************************************/
+void Moe_Reg_Malloc_Free(PF_MALLOC pfMalloc, PF_FREE pfFree);
+
+/******************************************************************************
+* Name       : void* Moe_Malloc(uint32 u32Size)
+* Function   : To be done.
+* Input      : None
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 25th May 2016
+******************************************************************************/
+void* Moe_Malloc(uint32 u32Size);
+
+/******************************************************************************
+* Name       : void Moe_Free(void *p)
+* Function   : To be done.
+* Input      : None
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 25th May 2016
+******************************************************************************/
+void Moe_Free(void *p);
+
+
+ 
+#if (0) /* Those nice try */
 #define __s(x)                        #x
 #define s(x)                        __s(x)
 
@@ -65,7 +189,6 @@ typedef void (*PF_POLL)(void);
 #define _MOE_NUNAME(a)    MARK_##a
 #define MOE_NUNAME        _MOE_NUNAME(69)
 
-
 #define MOE_EVT_ENTRY(evt)          case evt: goto MARK_##evt;  
 #define MOE_DELAY(v,evt)            Moe_Timer_Delay(v);sg_u16FP = evt; MOE_EVT_PROCESS(evt);  
 
@@ -75,10 +198,6 @@ typedef void (*PF_POLL)(void);
 #define TASK_EVENT_PROCESS_LIST_END    default:break;}
 
 
-
-
-
-
 #define PROCESS_NAME(x)             TASK_##x
 #define PROCESS(x)                  uint16 PROCESS_NAME(x)(uint8 u8Evt)
 
@@ -86,20 +205,10 @@ typedef void (*PF_POLL)(void);
 
 #define EVENT_PROCESS_BEGIN(x)      if(EVT_PARAM & x){
 
-#define EVENT_PROCESS_END(x)        return (EVT_PARAM ^ x);}                             
+#define EVENT_PROCESS_END(x)        return (EVT_PARAM ^ x);}    
 
+#endif
 
-
-void   Moe_Reg_Tasks(PF_TASK_PROCESS pfTaskFn);
-uint8  Moe_Memset(uint8* pDes, uint8 u8Val, uint8 u8Len);a
-void   Moe_Init();
-void   Moe_Run();
-uint8  Moe_Get_Acktive_Task();
-void   Moe_Reg_Malloc_Free();
-void*  Moe_Malloc(uint32 u32Size);
-void   Moe_Free(void *p);
-
- 
 #ifdef __cplusplus
 }
 #endif
