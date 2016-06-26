b6ff9d2fdbed50c23fd457d65ad404e4823d9252
Ian Hom <ianhom@163.com>
2016-06-26 12:17:19 +0800

    Update Task2.c

--- App/Task2/Task2.c
+++ App/Task2/Task2.c
@@ -12,13 +12,14 @@
 #include "type_def.h"
 #include "common_head.h"
 #include "project_config.h"
-#include "OSAL.h"
-#include "OSAL_Timer.h"
-#include "OSAL_Msg.h"
+#include "MOE_Core.h"
+#include "MOE_Event.h"
+#include "MOE_Timer.h"
+#include "MOE_Msg.h"
 #include "Task2.h"
 #include "debug.h"
 
-static uint16 Task2_Process(uint16 u16Evt);
+static uint16 Task2_Process(uint8 u8Evt);
 
 static uint8 sg_u8TaskID = TASK_NO_TASK;
 
@@ -36,10 +37,19 @@ static uint8 sg_u8TaskID = TASK_NO_TASK;
 void Task2_Init(uint8 u8TaskID)
 {
     sg_u8TaskID = u8TaskID;        /* Get the task ID */
-    Osal_Reg_Tasks(Task2_Process);
+    Moe_Reg_Tasks(Task2_Process);
     DBG_PRINT("Task 2 is inited successfully, Task ID is %d\n", sg_u8TaskID);
 
     /*--------------------   Add your init code here   ----------------------*/
+    T_TIMER tTmr;
+    tTmr.pfTmCallback = NULL;
+    tTmr.pPara        = NULL;
+    tTmr.u16Cnt       = 0Xffff;
+    tTmr.u16Evt       = EVENT_TIMER;
+    tTmr.u32TmOut     = 200;
+    tTmr.u8TaskID     = sg_u8TaskID;
+
+    Moe_Timer_Start(&tTmr);
 
     /*-------------------   The end of your init code   ---------------------*/
     return;
@@ -56,17 +66,26 @@ void Task2_Init(uint8 u8TaskID)
 * Author     : Ian
 * Date       : 3rd May 2016
 ******************************************************************************/
-static uint16 Task2_Process(uint16 u16Evt)
+static uint16 Task2_Process(uint8 u8Evt)
 {
     uint8  u8MsgType;
     void  *ptMsg;
+
+    if (u8Evt == EVENT_TIMER)
+    {
+        DBG_PRINT("I am task2\n");
+        goto MARK_EVENT_DELAY;
+    }
+    DBG_PRINT("haha\n");
+MARK_EVENT_DELAY: DBG_PRINT("wwwww\n");
+#if (0)
 
 /******************************************************************************/
 /* Process for message event                                                  */
 /******************************************************************************/
     EVENT_PROCESS_BEGIN(EVENT_MSG); 
     /*-----------------   Add your event process code here   -----------------*/
-        ptMsg = (void*)Osal_Msg_Receive(sg_u8TaskID, &u8MsgType);
+        ptMsg = (void*)Moe_Msg_Receive(sg_u8TaskID, &u8MsgType);
         while(ptMsg)
         {
             switch(u8MsgType)
@@ -80,10 +99,10 @@ static uint16 Task2_Process(uint16 u16Evt)
                     DBG_PRINT("I get a uint8 data 0x%x,0x%x,0x%x,0x%x!\n",(ptTestMsg->DATA.au8Data[0]),(ptTestMsg->DATA.au8Data[1]),(ptTestMsg->DATA.au8Data[2]),(ptTestMsg->DATA.au8Data[3]));
                     ptTestMsg->DATA.u32Data = 0xaabbccdd;
                     DBG_PRINT("Task %d change the message and forward it\n",sg_u8TaskID);
-                    Osal_Msg_Forward(ptMsg,2);
+                    Moe_Msg_Forward(ptMsg,2);
                 }
             }
-            ptMsg = (void*)Osal_Msg_Receive(sg_u8TaskID, &u8MsgType);
+            ptMsg = (void*)Moe_Msg_Receive(sg_u8TaskID, &u8MsgType);
         }
     /*----------------  The end of your event process code  ------------------*/
     EVENT_PROCESS_END(EVENT_MSG);
@@ -103,8 +122,11 @@ static uint16 Task2_Process(uint16 u16Evt)
 /******************************************************************************/
 
     return 0;
-}
 
+
+#endif
+
+}
 /* End of file */
 
 
