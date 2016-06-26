89d06211dfa5896d0055bc42ef51bfad1257d43e
Ian Hom <ianhom@163.com>
2016-06-26 12:17:45 +0800

    Update Task3.c

--- App/Task3/Task3.c
+++ App/Task3/Task3.c
@@ -12,13 +12,14 @@
 #include "type_def.h"
 #include "common_head.h"
 #include "project_config.h"
-#include "OSAL.h"
-#include "OSAL_Timer.h"
-#include "OSAL_Msg.h"
+#include "MOE_Core.h"
+#include "MOE_Event.h"
+#include "MOE_Timer.h"
+#include "MOE_Msg.h"
 #include "Task3.h"
 #include "debug.h"
 
-static uint16 Task3_Process(uint16 u16Evt);
+static uint16 Task3_Process(uint8 u8Evt);
 
 static uint8 sg_u8TaskID = TASK_NO_TASK;
 static uint8 sg_u8Cnt = 0;
@@ -37,7 +38,7 @@ static uint8 sg_u8Cnt = 0;
 void Task3_Init(uint8 u8TaskID)
 {
     sg_u8TaskID = u8TaskID;        /* Get the task ID */
-    Osal_Reg_Tasks(Task3_Process);
+    Moe_Reg_Tasks(Task3_Process);
     DBG_PRINT("Task 3 is inited successfully, Task ID is %d\n", sg_u8TaskID);
 
     /*--------------------   Add your init code here   ----------------------*/
@@ -58,7 +59,7 @@ void Task3_Init(uint8 u8TaskID)
 * Author     : Ian
 * Date       : 3rd May 2016
 ******************************************************************************/
-static uint16 Task3_Process(uint16 u16Evt)
+static uint16 Task3_Process(uint8 u8Evt)
 {
     uint8  u8MsgType;
     void  *ptMsg;
@@ -68,7 +69,7 @@ static uint16 Task3_Process(uint16 u16Evt)
 /******************************************************************************/
     EVENT_PROCESS_BEGIN(EVENT_MSG); 
     /*-----------------   Add your event process code here   -----------------*/
-        ptMsg = (void*)Osal_Msg_Receive(sg_u8TaskID, &u8MsgType);
+        ptMsg = (void*)Moe_Msg_Receive(sg_u8TaskID, &u8MsgType);
         while(ptMsg)
         {   sg_u8Cnt++;
             switch(u8MsgType)
@@ -78,12 +79,12 @@ static uint16 Task3_Process(uint16 u16Evt)
                     T_TEST_MSG *ptTestMsg = (T_TEST_MSG*)ptMsg;
                     DBG_PRINT("This is task %d\n",sg_u8TaskID);
                     DBG_PRINT("Get %d messages\n",sg_u8Cnt);
-                    //DBG_PRINT("I get a uint32 data 0x%x!\n",(ptTestMsg->DATA.u32Data));
-                    //DBG_PRINT("I get a uint16 data 0x%x,0x%x!\n",(ptTestMsg->DATA.au16Data[0]),(ptTestMsg->DATA.au16Data[1]));
-                    //DBG_PRINT("I get a uint8 data 0x%x,0x%x,0x%x,0x%x!\n\n",(ptTestMsg->DATA.au8Data[0]),(ptTestMsg->DATA.au8Data[1]),(ptTestMsg->DATA.au8Data[2]),(ptTestMsg->DATA.au8Data[3]));
+                    DBG_PRINT("I get a uint32 data 0x%x!\n",(ptTestMsg->DATA.u32Data));
+                    DBG_PRINT("I get a uint16 data 0x%x,0x%x!\n",(ptTestMsg->DATA.au16Data[0]),(ptTestMsg->DATA.au16Data[1]));
+                    DBG_PRINT("I get a uint8 data 0x%x,0x%x,0x%x,0x%x!\n\n",(ptTestMsg->DATA.au8Data[0]),(ptTestMsg->DATA.au8Data[1]),(ptTestMsg->DATA.au8Data[2]),(ptTestMsg->DATA.au8Data[3]));
                 }
             }
-            ptMsg = (void*)Osal_Msg_Receive(sg_u8TaskID, &u8MsgType);
+            ptMsg = (void*)Moe_Msg_Receive(sg_u8TaskID, &u8MsgType);
         }
     /*----------------  The end of your event process code  ------------------*/
     EVENT_PROCESS_END(EVENT_MSG);
