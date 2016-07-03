70eb9c9afd204b7618c69c84321aa9d86b131076
Ian Hom <ianhom@163.com>
2016-07-03 14:57:28 +0800

    Update MOE_main.c
    Remove hardware configuration

--- project/demo/MOE_main.c
+++ project/demo/MOE_main.c
@@ -12,45 +12,14 @@
 #include "MOE_Timer.h"
 #include "MOE_Msg.h"
 #include "MOE_Queue.h"
+#include "Board_Config.h"
 #include "KL25_Lpt_Time.h"
 #include "Btn_SM_Config.h"
 #include "Btn_SM_Module.h"
 
 /********************************************************************/
 
-uint8 Btn_St_Get(uint8 u8Ch)
-{
-    uint8 u8Temp;
-    if(u8Ch == 1)
-    {
-        u8Temp = (!(GPIOA_PDIR & (1 << 5)));
-    }
-    if(u8Ch == 2)
-    {
-        u8Temp = (!(GPIOA_PDIR & (1 << 4)));
-    }
-    if(u8Ch == 3)
-    {
-        u8Temp = (!(GPIOA_PDIR & (1 << 12)));
-    }
-    return  u8Temp;
-}
 
-uint16 System_Time(void)
-{
-    uint16 u16Temp = (uint16)App_GetSystemTime_ms();
-    return u16Temp;
-}
-
-void Gpio_Init()
-{
-    PORTA_PCR12 = PORT_PCR_MUX(0x1);
-    PORTA_PCR5  = PORT_PCR_MUX(0x1);
-    PORTA_PCR4  = PORT_PCR_MUX(0x1);
-    GPIOA_PDDR  &= ~(1 << 12);
-    GPIOA_PDDR  &= ~(1 << 5);
-    GPIOA_PDDR  &= ~(1 << 4);
-}
 
 void Poll_Process()
 {
@@ -63,7 +32,7 @@ int main (void)
     Gpio_Init();
     Timer_Init();
 
-    DBG_PRINT("\n***** MOE scheduler V0.01 *****\n");
+    DBG_PRINT("\n***** MOE scheduler V0.10 *****\n");
 
     Moe_Init(App_GetSystemTime_ms, Poll_Process);   
     
