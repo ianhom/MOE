ab904e9fa939c88b9c45e35fa959712c7b8953a1
Ian Hom <ianhom@163.com>
2016-07-03 14:39:37 +0800

    Create Board_Config.c

---
+++ project/demo/Board_Config.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+* File       : Board_Config.c
+* Function   : Hardware configurations
+* description: To be done.           
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd Jul 2016
+* History    :  No.  When           Who           What
+*               1    3/Jul/2016     Ian           Create
+******************************************************************************/
+
+#include "type_def.h"
+#include "common_head.h"
+#include "project_config.h"
+#include "MOE_Core.h"
+#include "MOE_Event.h"
+#include "MOE_Timer.h"
+#include "MOE_Msg.h"
+#include "Board_Config.h"
+#include "debug.h"
+
+
+/* End of file */
+
+
+
