a91151e377021271679d96322aec33a06ba621a2
Ian Hom <ianhom@163.com>
2016-07-03 14:56:02 +0800

    Update Board_Config.h

--- project/demo/Board_Config.h
+++ project/demo/Board_Config.h
@@ -27,6 +27,38 @@ extern "C" {
 #endif
 
 
+#define BOARD_CONFIG_BTN_ST_HIGH              (1)     /* The voltage state of pin is HIGH */
+#define BOARD_CONFIG_BTN_ST_LOW               (0)     /* The voltage state of pin is LOW  */
+
+
+/******************************************************************************
+* Name       : void Gpio_Init()
+* Function   : Init GPIOs
+* Input      : None
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd Jul 2016
+******************************************************************************/
+void Gpio_Init();
+
+/******************************************************************************
+* Name       : uint8 Btn_St_Get(uint8 u8Ch)
+* Function   : Get the current state of buttons.
+* Input      : uint8 u8Ch     1~255    The channel number of button
+* Output:    : None
+* Return     : BOARD_CONFIG_BTN_ST_HIGH  Pin with high voltage level
+*              BOARD_CONFIG_BTN_ST_LOW   Pin with low voltage level
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 3rd Jul 2016
+******************************************************************************/
+uint8 Btn_St_Get(uint8 u8Ch);
+
+
 
 
 #ifdef __cplusplus
