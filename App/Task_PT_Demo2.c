7236c989708447bff8fdac0b8ae14a98616e1d76
Ian Hom <ianhom@163.com>
2016-07-12 17:08:03 +0800

    Create Task_PT_Demo2.c

---
+++ App/Task_PT_Demo2.c
@@ -0,0 +1,60 @@
+/******************************************************************************
+* File       : Task_PT_Demo2.c
+* Function   : Another demo task for PT application
+* description: To be done.           
+* Version    : V1.00
+* Author     : Ian
+* Date       : 8th Jul 2016
+* History    :  No.  When           Who           What
+*               1    08/Jul/2016    Ian           Create
+******************************************************************************/
+
+#include "type_def.h"
+#include "common_head.h"
+#include "project_config.h"
+#include "MOE_Core.h"
+#include "MOE_Event.h"
+#include "MOE_Timer.h"
+#include "MOE_Msg.h"
+#include "MOE_PT.h"
+#include "Task_PT_Demo.h"
+#include "debug.h"
+
+static uint8 sg_u8TaskID = TASK_NO_TASK;
+
+/******************************************************************************
+* Name       : uint8 Task_PT_Demo2_Process(uint8 u8Evt)
+* Function   : A demo task for PT application
+* Input      : uint8 u8Evt  1~254     Event for the task
+* Output:    : None
+* Return     : SW_OK   Successful operation
+*            : SW_ERR  Failed operation
+*              1~254   Event which is not processed.
+* description: To be done
+* Version    : V1.00
+* Author     : Ian
+* Date       : 8th Jul 2016
+******************************************************************************/
+uint8 Task_PT_Demo2_Process(uint8 u8Evt)
+{    
+    PT_INIT();
+    
+    PT_BEGIN();
+
+    /******************************************************************/
+    MOE_MANDATORY_INIT();  /* Mandatory init, shout call it here only */
+    /******************************************************************/
+
+    while(1)
+    {
+        printf("I am another Task!!\n");
+        PT_DELAY(1000);
+    }
+
+    PT_END();
+
+    return SW_OK;
+
+}
+
+/* End of file */
