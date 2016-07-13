8106919cf470ae64a43fc9110f85e61993803332
Ian Hom <ianhom@163.com>
2016-07-13 17:43:43 +0800

    Update MOE_HAL_SPI.h

--- Driver/MCU/NXP-Freescale-_KLxx/MOE_HAL_SPI.h
+++ Driver/MCU/NXP-Freescale-_KLxx/MOE_HAL_SPI.h
@@ -26,8 +26,11 @@ extern "C" {
 #define __DEBUG_MODE      __DEBUG_MODE_MOE_HAL_SPI        /* According the set from project_config.h */
 #endif
 
+#define MOE_SPI_CS_HIGH           (1)     /* High level for SPI Cs pin */
+#define MOE_SPI_CS_LOW            (0)     /* Low level for SPI Cs pin  */
+
 /******************************************************************************
-* Name       : uint8 Moe_HAL_Init_SPI(void)
+* Name       : uint8 uint8 Moe_HAL_SPI_Init(void)
 * Function   : Init hardware abstract layer for SPI
 * Input      : None.
 * Output:    : None.
@@ -38,7 +41,49 @@ extern "C" {
 * Author     : Ian
 * Date       : 13th Jul 2016
 ******************************************************************************/
-uint8 Moe_HAL_Init_SPI(void);
+uint8 Moe_HAL_SPI_Init(void);
+
+/******************************************************************************
+* Name       : void Moe_HAL_SPI_Write(uint8 u8Data)
+* Function   : SPI write operation
+* Input      : uint8 u8Data  0~255    Data byte to be writen
+* Output:    : None.
+* Return     : None.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 13th Jul 2016
+******************************************************************************/
+void Moe_HAL_SPI_Write(uint8 u8Data);
+
+
+/******************************************************************************
+* Name       : uint8 Moe_HAL_SPI_Read(void)
+* Function   : SPI read operation
+* Input      : None.
+* Output:    : None.
+* Return     : uint8
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 13th Jul 2016
+******************************************************************************/
+uint8 Moe_HAL_SPI_Read(void);
+
+/******************************************************************************
+* Name       : void Moe_HAL_SPI_Write(uint8 ucData)
+* Function   : SPI write operation
+* Input      : uint8 u8Val    MOE_SPI_CS_LOW    Set Cs as low level
+*                             MOE_SPI_CS_HIGH   Set Cs as high level
+* Output:    : None.
+* Return     : None.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 13th Jul 2016
+******************************************************************************/
+void Moe_HAL_SPI_Cs_Ctrl(uint8 u8Val);
+
 
  
 #ifdef __cplusplus
