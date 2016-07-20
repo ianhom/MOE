c5ff02a23eb9d4cf93717758add970b56d80ae9c
Ian Hom <ianhom@163.com>
2016-07-20 21:49:36 +0800

    Update MOE_DRV_11xx.c

--- Driver/Dev/11xx/MOE_DRV_11xx.c
+++ Driver/Dev/11xx/MOE_DRV_11xx.c
@@ -19,6 +19,8 @@
 #include "MOE_DRV_11xx.h"
 #include "debug.h"
 
+static uint8 sg_au8RegRead[256] = {0};
+
 /******************************************************************************
 * Name       : uint8 Drv_11xx_Init(void)
 * Function   : Init operation for 11xx.
@@ -33,9 +35,48 @@
 ******************************************************************************/
 uint8 Drv_11xx_Init(void)
 {
+    uint8 u8Temp = 0;
+    
+    
+    Moe_HAL_Uart_Byte_Send(DRV_11XX_CFG_MODE);
+    if(DRV_11XX_RSPON = Moe_HAL_Uart_Byte_Receive())
+    {
+        Moe_HAL_Uart_Byte_Send(DRV_11XX_IDLE);
+    }
+    else
+    {
+        DBG_PRINT("Wrong operating mode of 11xx!!\n");
+    }
     return SW_OK;
 }
 
+/******************************************************************************
+* Name       : uint8 Drv_11xx_Simple_Cmd(uint8 u8Cmd)
+* Function   : Send command to 11xx
+* Input      : uint8 u8Cmd
+* Output:    : None.
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 20th Jul 2016
+******************************************************************************/
+uint8 Drv_11xx_Simple_Cmd(uint8 u8Cmd)
+{
+    Moe_HAL_Uart_Byte_Send(DRV_11XX_CFG_MODE);
+    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive())
+    {
+        return SW_ERR;
+    }
+    Moe_HAL_Uart_Byte_Send(u8Cmd);
+    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive())
+    {
+        return SW_ERR;
+    }
+    Moe_HAL_Uart_Byte_Send(DRV_11XX_IDLE);
+    return SW_OK;
+}
 
 
 
