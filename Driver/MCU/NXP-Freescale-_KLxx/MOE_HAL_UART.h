7fa57c066ce9cf9f322a8b90773b76faca6f208f
Ian Hom <ianhom@163.com>
2016-07-20 19:08:21 +0800

    Update MOE_HAL_UART.h

--- Driver/MCU/NXP-Freescale-_KLxx/MOE_HAL_UART.h
+++ Driver/MCU/NXP-Freescale-_KLxx/MOE_HAL_UART.h
@@ -26,6 +26,12 @@ extern "C" {
 #define __DEBUG_MODE      __DEBUG_MODE_MOE_HAL_UART       /* According the set from project_config.h */
 #endif
 
+#define MOE_HAL_UART_SND_BUF_FREE                   (1)   /* Uart sending buffer is free             */
+#define MOE_HAL_UART_SND_BUF_NOT_FREE               (0)   /* Uart sending buffer is NOT free         */
+
+#define MOE_HAL_UART_NO_RCV_DATA                    (0)   /* Uart get NONE received data             */
+#define MOE_HAL_UART_RCV_DATA                       (1)   /* Uart get a received data                */
+
 /******************************************************************************
 * Name       : uint8 Moe_HAL_Init_UART(void)
 * Function   : Init hardware abstract layer for UART
@@ -39,6 +45,60 @@ extern "C" {
 * Date       : 19th Jul 2016
 ******************************************************************************/
 uint8 Moe_HAL_UART_Init(void);
+
+/******************************************************************************
+* Name       : uint8 Moe_HAL_Uart_Byte_Receive()
+* Function   : Get a single byte data from uart receiving buffer
+* Input      : None.
+* Output:    : None.
+* Return     : uint8 Received data.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 20th Jul 2016
+******************************************************************************/
+uint8 Moe_HAL_Uart_Byte_Receive();
+
+/******************************************************************************
+* Name       : void Moe_HAL_Uart_Byte_Send(uint8 u8Data)
+* Function   : Send a single byte data to uart sending buffer.
+* Input      : None.
+* Output:    : None.
+* Return     : None.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 20th Jul 2016
+******************************************************************************/
+void Moe_HAL_Uart_Byte_Send(uint8 u8Data);
+
+/******************************************************************************
+* Name       : uint8 Moe_HAL_Uart_Free_Send_Buf(void)
+* Function   : Check sending buffer is free or NOT
+* Input      : None.
+* Output:    : None.
+* Return     : MOE_HAL_UART_SND_BUF_NOT_FREE  Sending buffer is NOT free
+*              MOE_HAL_UART_SND_BUF_FREE      Sending buffer is free
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 20th Jul 2016
+******************************************************************************/
+uint8 Moe_HAL_Uart_Free_Send_Buf(void);
+
+/******************************************************************************
+* Name       : uint8 Moe_HAL_Uart_Got_Data(void)
+* Function   : Check sending buffer is free or NOT
+* Input      : None.
+* Output:    : None.
+* Return     : MOE_HAL_UART_RCV_DATA     Get a data in receiving buffer
+*              MOE_HAL_UART_NO_RCV_DATA  Get NONE data in receiving buffer
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 20th Jul 2016
+******************************************************************************/
+uint8 Moe_HAL_Uart_Got_Data(void);
 
  
 #ifdef __cplusplus
