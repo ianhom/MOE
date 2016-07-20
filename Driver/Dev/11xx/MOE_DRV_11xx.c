d84305c28ec2e6471f034dc4991aa46ee6a222bb
Ian Hom <ianhom@163.com>
2016-07-20 19:16:19 +0800

    Create MOE_DRV_11xx.c

---
+++ Driver/Dev/11xx/MOE_DRV_11xx.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+* File       : MOE_DRV_11xx.c
+* Function   : Driver for 11xx.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 20th Jul 2016
+* History    :  No.  When           Who           What
+*               1    20/Jul/2016    Ian           Create
+******************************************************************************/
+
+#include "type_def.h"
+#include "common_head.h"
+#include "project_config.h"
+#include "MOE_Core.h"
+#include "MOE_Msg.h"
+#include "MOE_HAL.h"
+#include "MOE_HAL_UART.h"
+#include "MOE_DRV_11xx.h"
+#include "debug.h"
+
+/******************************************************************************
+* Name       : uint8 Drv_11xx_Init(void)
+* Function   : Init operation for 11xx.
+* Input      : None.
+* Output:    : None.
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 20th Jul 2016
+******************************************************************************/
+uint8 Drv_11xx_Init(void)
+{
+    return SW_OK;
+}
+
+
+
+
+/* end of file */
+
