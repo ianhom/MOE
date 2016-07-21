bb46bb7ec1185f5f7d882b14469308d3ae59ec4c
Ian Hom <ianhom@163.com>
2016-07-21 17:35:18 +0800

    Update MOE_DRV_11xx.c

--- Driver/Dev/11xx/MOE_DRV_11xx.c
+++ Driver/Dev/11xx/MOE_DRV_11xx.c
@@ -38,10 +38,10 @@ uint8 Drv_11xx_Init(void)
     uint8 u8Temp = 0;
     
     
-    Moe_HAL_Uart_Byte_Send(DRV_11XX_CFG_MODE);
-    if(DRV_11XX_RSPON = Moe_HAL_Uart_Byte_Receive())
+    Moe_HAL_Uart_Byte_Send(DRV_11XX_CMD_CFG_MODE);
+    if(DRV_11XX_CMD_RSPON = Moe_HAL_Uart_Byte_Receive())
     {
-        Moe_HAL_Uart_Byte_Send(DRV_11XX_IDLE);
+        Moe_HAL_Uart_Byte_Send(DRV_11XX_CMD_IDLE);
     }
     else
     {
@@ -50,11 +50,16 @@ uint8 Drv_11xx_Init(void)
     return SW_OK;
 }
 
+
 /******************************************************************************
-* Name       : uint8 Drv_11xx_Cmd(uint8 u8Cmd)
+* Name       : uint8 Drv_11xx_Cmd(uint8 u8Cmd, uint8 u8Para, uint8 u8Val, uint8 u8Len, uint8 *pu8Data)
 * Function   : Send command to 11xx
-* Input      : uint8 u8Cmd
-* Output:    : None.
+* Input      : uint8  u8Cmd    0~255                      Command code
+*              uint8  u8Para   DRV_11XX_CMD_WITHOUT_VAL   No value after command
+*                              DRV_11XX_CMD_WITH_VAL      A byte value after command
+*              uint8  u8Val    0~255                      Value after command
+*              uint8  u8Len    0~255                      Return bytes of command        
+* Output:    : uint8 *pu8Data                             Pointer to save return bytes     
 * Return     : SW_OK   Successful.
 *              SW_ERR  Failed.
 * description: To be done.
@@ -62,60 +67,50 @@ uint8 Drv_11xx_Init(void)
 * Author     : Ian
 * Date       : 20th Jul 2016
 ******************************************************************************/
-uint8 Drv_11xx_Cmd(uint8 u8Cmd)
+uint8 Drv_11xx_Cmd(uint8 u8Cmd, uint8 u8Para, uint8 u8Val, uint8 u8Len, uint8 *pu8Data)
 {
-    Moe_HAL_Uart_Byte_Send(DRV_11XX_CFG_MODE);
-    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive())
-    {
-        return SW_ERR;
-    }
-    Moe_HAL_Uart_Byte_Send(u8Cmd);
-    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive())
-    {
-        return SW_ERR;
-    }
-    Moe_HAL_Uart_Byte_Send(DRV_11XX_IDLE);
-    return SW_OK;
-}
+    uint8 u8Idx;
 
-/******************************************************************************
-* Name       : uint8 Drv_11xx_Simple_Cmd(uint8 u8Cmd)
-* Function   : Send command with 1 byte value to 11xx
-* Input      : uint8 u8Cmd    0~255    Command code
-*              uint8 u8Val    0~255    Value after command
-* Output:    : None.
-* Return     : SW_OK   Successful.
-*              SW_ERR  Failed.
-* description: To be done.
-* Version    : V1.00
-* Author     : Ian
-* Date       : 20th Jul 2016
-******************************************************************************/
-uint8 Drv_11xx_Cmd_1Val(uint8 u8Cmd, uint8 u8Val)
-{
-    Moe_HAL_Uart_Byte_Send(DRV_11XX_CFG_MODE);          /* Enter to configure mode */
-    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive())   /* Wait respons            */
+    /* Check if input parameter is invalid or NOT */
+    if((0 != u8Len) && (NULL == pu8Data))
+    {
+        DBG_PRINT("Invalid input\n");
+        return SW_ERR;
+    }
+
+    Moe_HAL_Uart_Byte_Send(DRV_11XX_CMD_CFG_MODE);          /* Enter to configure mode */
+    if(DRV_11XX_CMD_RSPON != Moe_HAL_Uart_Byte_Receive())   /* Wait respons            */
     {
         return SW_ERR;
     }
-    Moe_HAL_Uart_Byte_Send(u8Cmd);                      /* Send command first      */
-    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive())   /* Wait respons            */
+    Moe_HAL_Uart_Byte_Send(u8Cmd);                          /* Send command first      */
+    if(DRV_11XX_CMD_RSPON != Moe_HAL_Uart_Byte_Receive())   /* Wait respons            */
     {
         return SW_ERR;
     }
-    Moe_HAL_Uart_Byte_Send(u8Val);                      /* Send value              */
-    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive())   /* Wait respons            */
+    
+    if(DRV_11XX_CMD_WITH_VAL == u8Para)                     /* If need to send a value */
+    {
+        Moe_HAL_Uart_Byte_Send(u8Val);                      /* Send value              */
+    }
+        
+    for(u8Idx = 0; u8Idx < u8Len; u8Idx++)                  /* Get return data         */
+    {
+        pu8Data[u8Idx] = Moe_HAL_Uart_Byte_Receive();
+    }
+    
+    if(DRV_11XX_CMD_RSPON != Moe_HAL_Uart_Byte_Receive())   /* Wait respons            */
     {
         return SW_ERR;
     }
 
-    Moe_HAL_Uart_Byte_Send(DRV_11XX_IDLE);              /* Return to IDLE          */
+    Moe_HAL_Uart_Byte_Send(DRV_11XX_CMD_IDLE);              /* Return to IDLE          */
     return SW_OK;
 }
 
 
 /******************************************************************************
-* Name       : uint8 Drv_11xx_Bind(uint8 u8Ch, uint8 *pu8Addr)
+* Name       : uint8 Drv_11xx_Cmd_Bind(uint8 u8Ch, uint8 *pu8Addr)
 * Function   : Bind a sender SN address
 * Input      : uint8  u8Ch       1~64      Index of address
 *              uint8 *pu8Addr              Pointer for SN address
@@ -127,7 +122,7 @@ uint8 Drv_11xx_Cmd_1Val(uint8 u8Cmd, uint8 u8Val)
 * Author     : Ian
 * Date       : 20th Jul 2016
 ******************************************************************************/
-uint8 Drv_11xx_Bind(uint8 u8Ch, uint8 *pu8Addr)
+uint8 Drv_11xx_Cmd_Bind(uint8 u8Ch, uint8 *pu8Addr)
 {
     uint8 u8Idx;
 
@@ -138,35 +133,35 @@ uint8 Drv_11xx_Bind(uint8 u8Ch, uint8 *pu8Addr)
         return SW_ERR;
     }
 
-    Moe_HAL_Uart_Byte_Send(DRV_11XX_CFG_MODE);  /* Enter to configure mode */
-    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive()) /* Wait respons      */
+    Moe_HAL_Uart_Byte_Send(DRV_11XX_CMD_CFG_MODE);/* Enter to configure mode */
+    if(DRV_11XX_CMD_RSPON != Moe_HAL_Uart_Byte_Receive()) /* Wait respons    */
     {
         return SW_ERR;
     }
-    Moe_HAL_Uart_Byte_Send(u8Ch);              /* Select index             */
-    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive()) /* Wait respons      */
+    Moe_HAL_Uart_Byte_Send(u8Ch);              /* Select index               */
+    if(DRV_11XX_CMD_RSPON != Moe_HAL_Uart_Byte_Receive()) /* Wait respons    */
     {
         return SW_ERR;
     }
-    Moe_HAL_Uart_Byte_Send(0xFF);              /* First byte of address    */
-    Moe_HAL_Uart_Byte_Send(0x00);              /* Second byte of address   */
+    Moe_HAL_Uart_Byte_Send(0xFF);              /* First byte of address      */
+    Moe_HAL_Uart_Byte_Send(0x00);              /* Second byte of address     */
     for(u8Idx = 0; u8Idx < DRV_11XX_SN_LEN; u8Idx++)
     {
-        Moe_HAL_Uart_Byte_Send(pu8Addr[u8Idx]);/* SN address               */
+        Moe_HAL_Uart_Byte_Send(pu8Addr[u8Idx]);/* SN address                 */
     }
-    if(DRV_11XX_RSPON != Moe_HAL_Uart_Byte_Receive()) /* Wait respons      */
+    if(DRV_11XX_CMD_RSPON != Moe_HAL_Uart_Byte_Receive()) /* Wait respons    */
     {
         return SW_ERR;
     }
 
-    Moe_HAL_Uart_Byte_Send(DRV_11XX_IDLE);     /* Return back to IDLE      */
+    Moe_HAL_Uart_Byte_Send(DRV_11XX_CMD_IDLE);     /* Return back to IDLE    */
     return SW_OK;
 }
 
 
 
 /******************************************************************************
-* Name       : uint8 Drv_11xx_Mode_Sel(uint8 u8Mode)
+* Name       : uint8 Drv_11xx_Cmd_Mode_Sel(uint8 u8Mode)
 * Function   : Select operating mode 
 * Input      : uint8  u8Mode    DRV_11XX_MODE_S1    S1 sending only mode
 *                               DRV_11XX_MODE_S2    S2 bi-direction mode
@@ -178,7 +173,7 @@ uint8 Drv_11xx_Bind(uint8 u8Ch, uint8 *pu8Addr)
 * Author     : Ian
 * Date       : 20th Jul 2016
 ******************************************************************************/
-uint8 Drv_11xx_Mode_Sel(uint8 u8Mode)
+uint8 Drv_11xx_Cmd_Mode_Sel(uint8 u8Mode)
 {
     uint8 u8Idx;
 
@@ -190,7 +185,7 @@ uint8 Drv_11xx_Mode_Sel(uint8 u8Mode)
     }
 
     /* Write the command and value */
-    if(!Drv_11xx_Cmd_1Val(DRV_11XX_MODE_SEL_CMD, u8Mode))
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_MODE_SEL, DRV_11XX_CMD_WITH_VAL, u8Mode, 0, NULL))
     {
         DBG_PRINT("Something wrong with command!!\n");
         return SW_ERR;
@@ -199,7 +194,7 @@ uint8 Drv_11xx_Mode_Sel(uint8 u8Mode)
 }
 
 /******************************************************************************
-* Name       : uint8 Drv_11xx_Rcv_Mode(uint8 u8Mode)
+* Name       : uint8 Drv_11xx_Cmd_Rcv_Mode(uint8 u8Mode)
 * Function   : Select receving mode  
 * Input      : uint8  u8Mode    DRV_11XX_NORMAL_RCV    Receive normal telegram
 *                               DRV_11XX_ALL_RCV       Receive all telegram
@@ -211,7 +206,7 @@ uint8 Drv_11xx_Mode_Sel(uint8 u8Mode)
 * Author     : Ian
 * Date       : 20th Jul 2016
 ******************************************************************************/
-uint8 Drv_11xx_Rcv_Mode(uint8 u8Mode)
+uint8 Drv_11xx_Cmd_Rcv_Mode(uint8 u8Mode)
 {
     uint8 u8Idx;
 
@@ -221,9 +216,9 @@ uint8 Drv_11xx_Rcv_Mode(uint8 u8Mode)
         DBG_PRINT("Invalid input\n");
         return SW_ERR;
     }
-
+    
     /* Write the command and value */
-    if(!Drv_11xx_Cmd_1Val(DRV_11XX_RCV_MODE_CMD, u8Mode))
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_RCV_MODE, DRV_11XX_CMD_WITH_VAL, u8Mode, 0, NULL))
     {
         DBG_PRINT("Something wrong with command!!\n");
         return SW_ERR;
@@ -232,7 +227,7 @@ uint8 Drv_11xx_Rcv_Mode(uint8 u8Mode)
 }
 
 /******************************************************************************
-* Name       : uint8 Drv_11xx_Power_Sel(uint8 u8Lv)
+* Name       : uint8 Drv_11xx_Cmd_Power_Sel(uint8 u8Lv)
 * Function   : Select output power
 * Input      : uint8  u8Lv    1~5         Output level
 * Output:    : None.
@@ -243,7 +238,7 @@ uint8 Drv_11xx_Rcv_Mode(uint8 u8Mode)
 * Author     : Ian
 * Date       : 21th Jul 2016
 ******************************************************************************/
-uint8 Drv_11xx_Power_Sel(uint8 u8Lv)
+uint8 Drv_11xx_Cmd_Power_Sel(uint8 u8Lv)
 {
     uint8 u8Idx;
 
@@ -255,7 +250,7 @@ uint8 Drv_11xx_Power_Sel(uint8 u8Lv)
     }
 
     /* Write the command and value */
-    if(!Drv_11xx_Cmd_1Val(DRV_11XX_OUTPUT_PWR_CMD, u8Lv))
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_OUTPUT_PWR, DRV_11XX_CMD_WITH_VAL, u8Lv, 0, NULL))
     {
         DBG_PRINT("Something wrong with command!!\n");
         return SW_ERR;
@@ -263,6 +258,231 @@ uint8 Drv_11xx_Power_Sel(uint8 u8Lv)
     return SW_OK;
 }
 
+/******************************************************************************
+* Name       : uint8 Drv_11xx_Cmd_List_Bind(uint8 u8Num, uint8 *pu8Addr)
+* Function   : List a bind SN address
+* Input      : uint8  u8Num     1~64         Index of bind SN address
+* Output:    : uint8 *pu8Addr                Pointer to save SN address
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 21th Jul 2016
+******************************************************************************/
+uint8 Drv_11xx_Cmd_List_Bind(uint8 u8Num, uint8 *pu8Addr)
+{
+    uint8 u8Idx;
+
+    /* Check if the input parameters are invalid or NOT */
+    if(((u8Num - 1) > (DRV_11XX_MAX_BIND_ADDR - 1)) ||(NULL == pu8Addr))
+    {
+        DBG_PRINT("Invalid input\n");
+        return SW_ERR;
+    }
+
+    /* Write the command and value */
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_LIST_BIND, DRV_11XX_CMD_WITH_VAL, u8Num, 8, pu8Addr))
+    {
+        DBG_PRINT("Something wrong with command!!\n");
+        return SW_ERR;
+    }
+    return SW_OK;
+}
+
+/******************************************************************************
+* Name       : uint8 Drv_11xx_Cmd_Quality_Ind(uint8 *pu8Data)
+* Function   : Get the quality indicator value
+* Input      : None.
+* Output:    : uint8 *pu8Data                Pointer to save quality value
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 21th Jul 2016
+******************************************************************************/
+uint8 Drv_11xx_Cmd_Quality_Ind(uint8 *pu8Data)
+{
+    uint8 u8Idx;
+
+    /* Check if the input parameters are invalid or NOT */
+    if(NULL == pu8Data)
+    {
+        DBG_PRINT("Invalid input\n");
+        return SW_ERR;
+    }
+
+    /* Write the command */
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_QUALITY_IND, DRV_11XX_CMD_WITHOUT_VAL, 0, 1, pu8Data))
+    {
+        DBG_PRINT("Something wrong with command!!\n");
+        return SW_ERR;
+    }
+    return SW_OK;
+}
+
+/******************************************************************************
+* Name       : uint8 Drv_11xx_Cmd_Get_Rssi(uint8 *pu8Data)
+* Function   : Get the RSSI value
+* Input      : None.
+* Output:    : uint8 *pu8Data                Pointer to save RSSI value
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 21th Jul 2016
+******************************************************************************/
+uint8 Drv_11xx_Cmd_Get_Rssi(uint8 *pu8Data)
+{
+    uint8 u8Idx;
+
+    /* Check if the input parameters are invalid or NOT */
+    if(NULL == pu8Data)
+    {
+        DBG_PRINT("Invalid input\n");
+        return SW_ERR;
+    }
+
+    /* Write the command */
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_RSSI, DRV_11XX_CMD_WITHOUT_VAL, 0, 1, pu8Data))
+    {
+        DBG_PRINT("Something wrong with command!!\n");
+        return SW_ERR;
+    }
+    return SW_OK;
+}
+
+/******************************************************************************
+* Name       : uint8 Drv_11xx_Cmd_Reg_Read(uint8 *pu8Data)
+* Function   : Get the all register value
+* Input      : None.
+* Output:    : uint8 *pu8Data                Pointer to register value
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 21th Jul 2016
+******************************************************************************/
+uint8 Drv_11xx_Cmd_Reg_Read(uint8 *pu8Data)
+{
+    uint8 u8Idx;
+
+    /* Check if the input parameters are invalid or NOT */
+    if(NULL == pu8Data)
+    {
+        DBG_PRINT("Invalid input\n");
+        return SW_ERR;
+    }
+
+    /* Write the command */
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_REG_READ, DRV_11XX_CMD_WITHOUT_VAL, 0, DRV_11XX_MAX_REG_NUM, pu8Data))
+    {
+        DBG_PRINT("Something wrong with command!!\n");
+        return SW_ERR;
+    }
+    return SW_OK;
+}
+
+/******************************************************************************
+* Name       : uint8 Drv_11xx_Cmd_Get_Des_Addr_Tab (uint8 *pu8Data)
+* Function   : Get the destination address table
+* Input      : None.
+* Output:    : uint8 *pu8Data                Pointer to register value
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 21th Jul 2016
+******************************************************************************/
+uint8 Drv_11xx_Cmd_Get_Des_Addr_Tab (uint8 *pu8Data)
+{
+    uint8 u8Idx;
+
+    /* Check if the input parameters are invalid or NOT */
+    if(NULL == pu8Data)
+    {
+        DBG_PRINT("Invalid input\n");
+        return SW_ERR;
+    }
+
+    /* Write the command */
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_DES_ADDR_TAB, DRV_11XX_CMD_WITHOUT_VAL, 0, DRV_11XX_CMD_DES_ADDR_TAB_LEN, pu8Data))
+    {
+        DBG_PRINT("Something wrong with command!!\n");
+        return SW_ERR;
+    }
+    return SW_OK;
+}
+
+/******************************************************************************
+* Name       : uint8 Drv_11xx_Cmd_Get_All_Addr_Tab (uint8 *pu8Data)
+* Function   : Get the all address table
+* Input      : None.
+* Output:    : uint8 *pu8Data                Pointer to register value
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 21th Jul 2016
+******************************************************************************/
+uint8 Drv_11xx_Cmd_Get_All_Addr_Tab (uint8 *pu8Data)
+{
+    uint8 u8Idx;
+
+    /* Check if the input parameters are invalid or NOT */
+    if(NULL == pu8Data)
+    {
+        DBG_PRINT("Invalid input\n");
+        return SW_ERR;
+    }
+
+    /* Write the command */
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_ALL_ADDR_TAB, DRV_11XX_CMD_WITHOUT_VAL, 0, DRV_11XX_CMD_ALL_ADDR_TAB_LEN, pu8Data))
+    {
+        DBG_PRINT("Something wrong with command!!\n");
+        return SW_ERR;
+    }
+    return SW_OK;
+}
+
+
+
+/******************************************************************************
+* Name       : uint8 Drv_11xx_Cmd_Reg_Read(uint8 *pu8Data)
+* Function   : Get the all register value
+* Input      : None.
+* Output:    : uint8 *pu8Data                Pointer to register value
+* Return     : SW_OK   Successful.
+*              SW_ERR  Failed.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 21th Jul 2016
+******************************************************************************/
+uint8 Drv_11xx_Cmd_Channel_Sel(uint8 u8Ch)
+{
+    uint8 u8Idx;
+
+    /* Check if the input parameters are invalid or NOT */
+    if((u8Ch - 1) > 2)
+    {
+        DBG_PRINT("Invalid input\n");
+        return SW_ERR;
+    }
+
+    /* Write the command and value */
+    if(!Drv_11xx_Cmd(DRV_11XX_CMD_CH_SEL, DRV_11XX_CMD_WITH_VAL, u8Ch, 0, NULL))
+    {
+        DBG_PRINT("Something wrong with command!!\n");
+        return SW_ERR;
+    }
+    return SW_OK;
+}
 
 /* end of file */
 
