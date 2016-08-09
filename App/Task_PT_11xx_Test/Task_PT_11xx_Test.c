ebf3ab339f3a0b7abca82e860956aedd22a6e157
Ian Hom <ianhom@163.com>
2016-08-09 17:21:22 +0800

    Update Task_PT_11xx_Test.c

--- App/Task_PT_11xx_Test/Task_PT_11xx_Test.c
+++ App/Task_PT_11xx_Test/Task_PT_11xx_Test.c
@@ -24,7 +24,8 @@
 #include "Board_Config.h"
 
 static uint8 sg_u8TaskID = TASK_NO_TASK;
-
+static uint8 sg_u8LFNIdx = 0;
+static uint8 sg_au8LFN[] = {0x80, 0x82, 0x84, 0x86, 0x88, 0x8A, 0x8C, 0x8E};
 static uint8 sg_au8SndTele[]   = {0x09, 0x00, 0x11, 0x22, 0x33, 0x44, 0x01, 0x00, 0x00, 0x00};
 static uint8 sg_au8RcvTele[50] = {0x00};
 
@@ -53,7 +54,7 @@ static void (*const TASK_PT_DEMO_LED_On)(uint8 u8Clr)     = TASK_PT_DEMO_LED_ON;
 uint8 Task_PT_11xx_Test(uint8 u8Evt)
 {   
     uint16 u16Idx;
-    uint8  u8Temp = 1;
+    uint8  u8Temp = 0x07;
     
     PT_INIT();
     
@@ -64,9 +65,10 @@ uint8 Task_PT_11xx_Test(uint8 u8Evt)
     /******************************************************************/
 
     Drv_11xx_Init();
-    //Moe_HAL_Uart_Byte_Receive();
+//    Moe_HAL_Uart_Byte_Receive();
     //Drv_11xx_Write_Reg(DRV_11XX_REG_TM_OUT, 1, &u8Temp);
-
+//    Drv_11xx_Write_Reg(0x60, 1, &u8Temp);
+     
     Drv_11xx_Cmd_Reg_Read(sg_au8RegVal);
     for (u16Idx = 0; u16Idx < DRV_11XX_MAX_REG_NUM; u16Idx++)
     {
@@ -76,16 +78,18 @@ uint8 Task_PT_11xx_Test(uint8 u8Evt)
     Moe_HAL_Uart_Rx_Int_Enable();
     while(1)
     {
-#if (0)
-        TASK_PT_DEMO_LED_On(BOARD_CONFIG_LED_BLUE);
-        Drv_11xx_Receive_Telegram(sg_au8RcvTele);
-        TASK_PT_DEMO_LED_Off(BOARD_CONFIG_LED_BLUE);
-        PT_DELAY(500);
+#if (1)
+        //TASK_PT_DEMO_LED_On(BOARD_CONFIG_LED_BLUE);
+        //Drv_11xx_Receive_Telegram(sg_au8RcvTele);
+        //TASK_PT_DEMO_LED_Off(BOARD_CONFIG_LED_BLUE);
+        //PT_DELAY(500);
         TASK_PT_DEMO_LED_On(BOARD_CONFIG_LED_GREEN);
-        //Drv_11xx_Send_Telegram(sg_au8SndTele);
-        Drv_11xx_Send_Telegram(sg_au8RcvTele);
-        PT_DELAY(500);
+        Drv_11xx_Send_Telegram(sg_au8SndTele);
+        //Drv_11xx_Send_Telegram(sg_au8RcvTele);
+        PT_DELAY(50);
         sg_au8SndTele[9]++;
+        sg_au8SndTele[6] = sg_au8LFN[sg_u8LFNIdx];
+        sg_u8LFNIdx = ((sg_u8LFNIdx + 1)%8);
         TASK_PT_DEMO_LED_Off(BOARD_CONFIG_LED_GREEN);
 #else
         TASK_PT_DEMO_LED_On(BOARD_CONFIG_LED_BLUE);
@@ -99,7 +103,7 @@ uint8 Task_PT_11xx_Test(uint8 u8Evt)
 //            PT_DELAY(100);
 //            TASK_PT_DEMO_LED_Off(BOARD_CONFIG_LED_GREEN);
         }
-        PT_DELAY(1);
+        PT_BREAK();
 #endif
     }
 
