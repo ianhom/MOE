5afc2d6d0ec94573e08c7f3914cb4041aa26e0ca
Ian Hom <ianhom@163.com>
2016-09-09 17:42:47 +0800

    Update MOE_Msg.c

--- Core/MOE_Msg.c
+++ Core/MOE_Msg.c
@@ -18,13 +18,11 @@
 #include "debug.h"
 
 
-static T_MSG_HEAD* Moe_Msg_Create(uint8 *pu8DestTask, uint8 u8MsgType, uint16 u16Size, void *ptMsg);
+static T_MSG_HEAD* Moe_Msg_Create(uint8 u8DestTask, uint8 u8MsgType, uint16 u16Size, void *ptMsg);
 static T_MSG_HEAD* Moe_Msg_Del(T_MSG_HEAD *ptMsg);
 static uint16 Moe_Msg_Max_Cnt(void);
 
 
-static T_MSG_HEAD  *sg_ptMsgListHead  = NULL;                     /* Head node of messages      */ 
-static T_MSG_HEAD  *sg_ptMsgListTail  = NULL;                     /* Tail node of messages      */
 static uint16       sg_u16MsgPollFlag = MOE_MSG_POLL_NONE;        /* Message poll request flag  */
 
 static uint8        sg_au8RcvDone[MAX_TASK_NUM] = {0};
@@ -61,7 +59,7 @@ void Moe_Msg_Init(void)
 * Author     : Ian
 * Date       : 26th May 2016
 ******************************************************************************/
-static T_MSG_HEAD* Moe_Msg_Create(uint8 *pu8DestTask, uint8 u8MsgType, uint16 u16Size, void *ptMsg)
+static T_MSG_HEAD* Moe_Msg_Create(uint8 u8DestTask, uint8 u8MsgType, uint16 u16Size, void *ptMsg)
 {
     uint32 u32IntSt;
     T_MSG_HEAD *ptMsgHead;
@@ -75,30 +73,26 @@ static T_MSG_HEAD* Moe_Msg_Create(uint8 *pu8DestTask, uint8 u8MsgType, uint16 u1
 
     if(NULL != ptMsgHead)
     {
-        DBG_PRINT("Create a message successfully!!\n");
-        ptMsgHead->ptNext     = NULL;         
-        ptMsgHead->u16Size    = u16Size;
+        DBG_PRINT("Create a message successfully!!\n");      
         ptMsgHead->u8MsgType  = u8MsgType;
         ptMsgHead->u8SrcTask  = TASK_CURRENT_TASK;
-        if(TASK_ALL_TASK == *pu8DestTask)
+        if(TASK_ALL_TASK == u8DestTask)
         {
             ptMsgHead->u8CopyCnt  = MAX_TASK_NUM - 1;     /* Message for all tasks                           */
 #ifdef __WANTED_A_LIVE_FOX
             ptMsgHead->u8DestTask = TASK_CURRENT_TASK % TASK_LAST_TASK + 1; /* Start with the next task      */
-            *pu8DestTask = ptMsgHead->u8DestTask;
             DBG_PRINT("Fox is ready to kill the next one!!\n");
 #else
             /* Check if it is the first task which sends a message to other tasks */
             /* If so, start with the second task                                  */
             /* Otherwise, start with the first task                               */
             ptMsgHead->u8DestTask = TASK_FIRST_TASK + (!(TASK_CURRENT_TASK - TASK_FIRST_TASK));
-            *pu8DestTask = ptMsgHead->u8DestTask;
 #endif        
         }
         else
-        {
-            ptMsgHead->u8DestTask = *pu8DestTask;
+        {            
             ptMsgHead->u8CopyCnt  = 0;                    /* Message for single tasks                        */
+            ptMsgHead->u8DestTask = u8DestTask;
         }
 #ifdef __FLEXIBLE_ARRAY_NOT_SUPPORTED                     /* If the complier does NOT support flexible array */
         {
@@ -141,141 +135,30 @@ static T_MSG_HEAD* Moe_Msg_Create(uint8 *pu8DestTask, uint8 u8MsgType, uint16 u1
 ******************************************************************************/
 uint8 Moe_Msg_Send(uint8 u8DestTask, uint8 u8MsgType, uint16 u16Size, void *ptMsg)
 {    
-    uint32 u32IntSt;
     T_MSG_HEAD *ptMsgNode;
        
     /* Check if the destination task is valid or NOT */
-    MOE_CHECK_IF_RET_ST((MAX_TASK_NUM  <  u8DestTask)\
-                     && (TASK_ALL_TASK != u8DestTask)\
-                     || (TASK_NO_TASK  == u8DestTask)),\
-                       "The destination task of the sending message should be valid!!\n");
+    MOE_CHECK_IF_RET_ST(((MAX_TASK_NUM  >  u8DestTask)\
+                      && (TASK_ALL_TASK != u8DestTask)\
+                      || (TASK_NO_TASK  == u8DestTask)),\
+                          "The destination task of the sending message should be valid!!\n");
 
     /* If the length of message is less then a message head */
-    MOE_CHECK_IF_RET_ST((0 ==u16Size),"The length of creating message is 0\n");
+    MOE_CHECK_IF_RET_ST((0 == u16Size),"The length of creating message is 0\n");
 
     /* Check if the message pointer is valid or NOT */
     MOE_CHECK_IF_RET_ST((ptMsg == NULL),"The message pointer should NOT be NULL\n");
 
-    ptMsgNode = Moe_Msg_Create(&u8DestTask, u8MsgType, u16Size, ptMsg);
+    ptMsgNode = Moe_Msg_Create(u8DestTask, u8MsgType, u16Size, ptMsg);
     
-    MOE_CHECK_IF_RET_ST((NULL == ptMsgNode),"Message is NOT created!!\n");
+    MOE_CHECK_IF_RET_ST((NULL == ptMsgNode), "Message is NOT created!!\n");
 
-
-    ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
-    /**************************************************************************************************/
-    if(NULL == sg_ptMsgListHead)                 /* If there is NO nodes            */
-    {
-        sg_ptMsgListHead = ptMsgNode;            /* Add new node as the fisrt one   */
-    }
-    else                                         /* If node exsits                  */
-    {
-        sg_ptMsgListTail->ptNext = ptMsgNode;    /* Add new node after the tail one */
-    }
-    sg_ptMsgListTail = ptMsgNode;                /* Update the tail node            */
-    /**************************************************************************************************/
-    EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
-    
-    Moe_Event_Set(u8DestTask,EVENT_MSG,MOE_EVENT_NORMAL);/* Set a message event to call destination task      */
+    Moe_Event_Set(ptMsgNode->u8DestTask, EVENT_MSG, MOE_EVENT_NORMAL, ptMsgNode);/* Set a message event to call destination task      */
     
     DBG_PRINT("Message is sent successfully!!\n");
     return SW_OK;
 }
 
-
-/******************************************************************************
-* Name       : uint8* Moe_Msg_Receive(uint8 u8DestTask, uint8 *pu8Type)
-* Function   : Receive a message
-* Input      : uint8  u8DestTask    1~254     The destination task number
-* Output:    : uint8 *pu8Type       0~255     Type of message
-* Return     : NULL  Failed.
-*              The pointer of the message 
-* description: To be done
-* Version    : V1.00
-* Author     : Ian
-* Date       : 31st May 2016
-******************************************************************************/
-uint8* Moe_Msg_Receive(uint8 u8DestTask, uint8 *pu8Type)
-{
-    T_MSG_HEAD *ptFind  = sg_ptMsgListHead;
-    T_MSG_HEAD *ptFound = NULL;
-
-    /* Check if the task is valid or NOT                                     */
-    /* The Destination task should NOT be TASK_NO_TASK, that is meaningless  */
-    /* The Destination task should NOT be bigger than the max task number    */
-    MOE_CHECK_IF_RET_VAL((TASK_NO_TASK == u8DestTask ) || (u8DestTask > MAX_TASK_NUM), NULL, "Message receiving task ID should be valid!!\n");
-  
-    /* Check if the pointer is invalid or NOT */    
-    MOE_CHECK_IF_RET_VAL((pu8Type == NULL), NULL, "Message type pointer should NOT be NULL\n");
-
-    sg_au8RcvDone[u8DestTask - 1] = MOE_MSG_RCV_DONE;
-
-    /* Try to find the message */
-    while(ptFind != NULL)
-    {
-        if(ptFind->u8DestTask == u8DestTask)           /* If the message is found            */
-        {
-            DBG_PRINT("A new message is found!!\n");
-            ptFound = ptFind;                          /* Set such as the one we found       */
-            break;                                     /* Stop following message checking    */
-        }
-        ptFind = ptFind->ptNext;                       /* Continue check next message        */
-    }
-
-    /* If we have found a message */
-    if(NULL != ptFound)
-    {
-        *pu8Type = ptFound->u8MsgType;                 /* Output the type of message         */
-        
-        /* If such message is for all task and there are more than 1 forwarding */
-        if(ptFound->u8CopyCnt > 1)
-        {
-            ptFound->u8CopyCnt--;                                        /* Count down the copy count  */
-#ifdef __WANTED_A_LIVE_FOX         
-            ptFound->u8DestTask = TASK_CURRENT_TASK % TASK_LAST_TASK + 1;/* Stop forward such message  */
-            DBG_PRINT("Fox just killed %d!!\n", TASK_CURRENT_TASK);
-#else
-            /* If the next task is the one which sent this message */
-            if(ptFound->u8DestTask == ptFound->u8SrcTask)
-            {
-                ptFound->u8DestTask += 2;                      /* Forward to the next task   */
-                DBG_PRINT("It is a self-message send by %d, ignore!!\n", TASK_CURRENT_TASK);
-            }
-            else
-            {
-                ptFound->u8DestTask++;                         /* Forward to the next task   */  
-            }                     
-#endif
-            Moe_Event_Set(ptFound->u8DestTask,EVENT_MSG,MOE_EVENT_NORMAL);/* Set MSG event to next task */
-            DBG_PRINT("The message to all tasks is processed by task %d, ready for next forwarding!!\n", TASK_CURRENT_TASK);
-        }
-                
-        /* If such message is for all task and this is the last forwarding */
-        /* Or it is a message for a single task                            */
-        else
-        {
-            ptFound->u8DestTask = TASK_NO_TASK;         /* Stop forwarding message            */
-            sg_u16MsgPollFlag++;                        /* Set flag to poll message process   */
-
-            /* If it is a message for all tasks */
-            if(0 != ptFound->u8CopyCnt)
-            {
-#ifdef __WANTED_A_LIVE_FOX
-                DBG_PRINT("Fox killed all except self %d!!\n", ptFound->u8SrcTask);                    
-#endif
-                DBG_PRINT("All tasks have received the message!!\n");
-            } 
-        }
-
-#ifdef __FLEXIBLE_ARRAY_NOT_SUPPORTED
-        return (uint8*)(ptFound + 1);                   /* Return the data of message         */
-#else
-        return (uint8*)ptFound->au8Data;                /* Return the data of message         */
-#endif        
-    }
-   
-    DBG_PRINT("No message is found for task %d!!\n", u8DestTask);
-    return NULL;
-}
 
 /******************************************************************************
 * Name       : uint8 Moe_Msg_Forward(void *ptMsg, uint8 u8NextTask)
@@ -293,10 +176,8 @@ uint8* Moe_Msg_Receive(uint8 u8DestTask, uint8 *pu8Type)
 * Author     : Ian
 * Date       : 5th Jun 2016
 ******************************************************************************/
-uint8 Moe_Msg_Forward(void *ptMsg, uint8 u8NextTask)
+uint8 Moe_Msg_Forward(T_MSG_HEAD *ptMsg, uint8 u8NextTask)
 {
-    T_MSG_HEAD *ptFind;
-
     /* Check if the pointer is invalid or NOT */
     if(NULL == ptMsg)
     {
@@ -313,19 +194,17 @@ uint8 Moe_Msg_Forward(void *ptMsg, uint8 u8NextTask)
         return SW_ERR;
     }
 
-    ptFind = ((T_MSG_HEAD*)ptMsg - 1);   /* Get the head of the message */
-
     /* If it is a message to all task, do NOT forward */
-    if(0 != ptFind->u8CopyCnt)
+    if(0 != ptMsg->u8CopyCnt)
     {
         DBG_PRINT("It is a message to all task, do NOT forward!!\n");
         return SW_ERR;
     }
 
     /* Set the next task to receive such message */
-    ptFind->u8DestTask = u8NextTask;
-    Moe_Event_Set(u8NextTask,EVENT_MSG,MOE_EVENT_NORMAL); /* Call next task to receive message  */
-    sg_u16MsgPollFlag--;                                  /* Decrease count for poll message    */
+    ptMsg->u8DestTask = u8NextTask;
+    ptMsg->u8SrcTask  = TASK_CURRENT_TASK;
+    Moe_Event_Set(u8NextTask, EVENT_MSG, MOE_EVENT_NORMAL, ptMsg); /* Call next task to receive message  */
     DBG_PRINT("The message is forwarded to task %d\n",u8NextTask);
     return SW_OK;
 }
@@ -366,77 +245,50 @@ static T_MSG_HEAD* Moe_Msg_Del(T_MSG_HEAD *ptMsg)
 * Author     : Ian
 * Date       : 31st May 2016
 ******************************************************************************/
-uint8 Moe_Msg_Process(void)
+uint8 Moe_Msg_Process(T_MSG_HEAD *ptMsg)
 {
-    T_MSG_HEAD *ptFind,*ptFound = NULL;
-    T_MSG_HEAD *ptMsg;
-    uint32      u32IntSt;
-    
-    /* If it is unnecessary to process message */
-    if(MOE_MSG_POLL_NONE == sg_u16MsgPollFlag)
-    {   /* Return */
+    if(ptMsg->u8CopyCnt)
+    {
+        ptMsg->u8CopyCnt--;
+        if(ptMsg->u8CopyCnt)
+        {
+#ifdef __WANTED_A_LIVE_FOX         
+            ptMsg->u8DestTask = TASK_CURRENT_TASK % TASK_LAST_TASK + 1;/* Stop forward such message  */
+            DBG_PRINT("Fox just killed %d!!\n", TASK_CURRENT_TASK);
+#else
+            /* If the next task is the one which sent this message */
+            if(ptMsg->u8DestTask == ptMsg->u8SrcTask)
+            {
+                ptMsg->u8DestTask += 2;                      /* Forward to the next task   */
+                DBG_PRINT("It is a self-message send by %d, ignore!!\n", TASK_CURRENT_TASK);
+            }
+            else
+            {
+                ptMsg->u8DestTask++;                         /* Forward to the next task   */  
+            }                     
+#endif
+            Moe_Event_Set(ptMsg->u8DestTask, EVENT_MSG, MOE_EVENT_NORMAL, ptMsg);/* Set MSG event to next task */
+            DBG_PRINT("The message to all tasks is processed by task %d, ready for next forwarding!!\n", TASK_CURRENT_TASK);
+        }
+                        
+        /* If such message is for all task and this is the last forwarding */
+        /* Or it is a message for a single task                            */
+        else
+        {        
+            /* If it is a message for all tasks */
+            if(0 != ptMsg->u8CopyCnt)
+            {
+#ifdef __WANTED_A_LIVE_FOX
+                DBG_PRINT("Fox killed all except herself %d!!\n", ptMsg->u8SrcTask);                    
+#endif
+                DBG_PRINT("All tasks have received the message!!\n");
+            } 
+            MOE_FREE(ptMsg);
+        }
         return SW_OK;
     }
 
-    /* Else, need to process message */    
-
-    DBG_PRINT("Running message process!!\n");
-
-    ptFind = sg_ptMsgListHead;
-    while(ptFind != NULL)
-    {   
-        /* If we find a message with no destination task, detele it */
-        if(TASK_NO_TASK == ptFind->u8DestTask)
-        {
-            ptFound = ptFind;                                   /* Record the deleting node    */
-            ENTER_CRITICAL_ZONE(u32IntSt);  /* Enter the critical zone to prevent event updating unexpectedly */
-            /**************************************************************************************************/
-            /* If such message is the first one */
-            if(ptFind == sg_ptMsgListHead)
-            {
-                if(ptFind == sg_ptMsgListTail)                   /* And it is also the last one */
-                {
-                    sg_ptMsgListHead = NULL;                     
-                    sg_ptMsgListTail = NULL;                     /* There is no message         */
-                }
-                else                                             /* If it is NOT the last one   */
-                {
-                    sg_ptMsgListHead = sg_ptMsgListHead->ptNext; /* Make next one as the head   */
-                }
-            }
-            else/* If such message is NOT the first one */                                                  
-            {
-                ptMsg = sg_ptMsgListHead;                        /* Start with the first one    */
-                while(ptMsg != NULL)                             
-                {
-                    if(ptMsg->ptNext == ptFind)                  /* If we find the previous one */
-                    {
-                        ptMsg->ptNext = ptFind->ptNext;          /* Delete message from list    */
-                        if(ptMsg->ptNext == NULL)                
-                        {
-                            sg_ptMsgListTail = ptMsg;            /* Update the tail message     */
-                        }
-                        break;
-                    }
-                    ptMsg = ptMsg->ptNext;                       /* Check next one              */
-                }
-            }
-            /**************************************************************************************************/
-            EXIT_CRITICAL_ZONE(u32IntSt);   /* Exit the critical zone                                         */
-            
-            sg_u16MsgPollFlag--;            /* decrease the polling count */
-        } 
-        ptFind = ptFind->ptNext;
-
-        /* If the delete  */
-        if(NULL != ptFound)
-        {
-            Moe_Msg_Del(ptFound);           /* Free the deleting node     */
-            DBG_PRINT("The deleting node is free!!\n");
-            ptFound = NULL;
-        }
-    }
-    return SW_OK;
+    return SW_ERR;
 }
 
 
