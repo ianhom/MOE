a8338d66d1af501d366b0de30c4eb60791ec6e91
Ian Hom <ianhom@163.com>
2016-11-06 18:02:36 +0800

    Update project_config.h

--- project/PT_Demo_KL25/project_config.h
+++ project/PT_Demo_KL25/project_config.h
@@ -51,7 +51,7 @@ LIST_OF_REG_TASK
 #undef REG_TASK
 };
 
-#define MAX_TASK_NUM                         (sizeof(cg_apfTaskFn)/sizeof(cg_apfTaskFn[0]))      /* Max number of tasks                      */
+#define MAX_TASK_NUM                         (sizeof(cg_apfTaskFn)/sizeof(cg_apfTaskFn[0]))      /* Max number of tasks */
 
 #define MAX_QUEUE_EVT_NUM                    (MAX_TASK_NUM * 10)
  
@@ -66,45 +66,42 @@ LIST_OF_REG_TASK
 //#define __MOE_SPI_ENABLE
 #define __MOE_UART_ENABLE
 
-/*********************************************************
+/******************************************************************************
 NOTE: You have four ways to use malloc:
-1. Do not define __MALLOC_OPTION or define __MALLOC_OPTION
-   as __MALLOC_STD, and Do not re-write the malloc and 
-   free function, you will use the standard malloc and
-   free function in lib.
-2. Do not define __MALLOC_OPTION or define __MALLOC_OPTION
-   as __MALLOC_STD, and re-write the malloc and free 
-   function, you will use your malloc and free function, 
-   Please NOTE that the whole project use your malloc and 
-   free function
-3. Define __MALLOC_OPTION as __MALLOC_MY, now your can use
-   your own malloc and free function, however, if you use
-   the same name with "malloc" and "free", whole project
-   use your malloc and free just like situation 2.
-4. Define __MALLOC_OPTION as __MALLOC_MOE, the Moe_Malloc
-   and Moe_Free will be used.
+1. Do not define __MALLOC_OPTION or define __MALLOC_OPTION as __MALLOC_STD, 
+   and Do not re-write the malloc and free function, you will use the standard 
+   malloc and free function in lib.
+2. Do not define __MALLOC_OPTION or define __MALLOC_OPTION as __MALLOC_STD, 
+   and re-write the malloc and free function, you will use your malloc and free
+   function, please NOTE that the whole project use your malloc and free 
+   function.
+3. Define __MALLOC_OPTION as __MALLOC_MY, now your can use your own malloc and
+   free function, however, if you use the same name with "malloc" and "free", 
+   whole project use your malloc and free just like situation 2.
+4. Define __MALLOC_OPTION as __MALLOC_MOE, the Moe_Malloc and Moe_Free will be 
+   used.
 Options for __MALLOC_OPTION:
     __MALLOC_STD             Use standard malloc and free 
-    __MALLOC_MOE            Use MOE malloc and free     
+    __MALLOC_MOE             Use MOE malloc and free     
     __MALLOC_MY              Use your malloc and free     
-**********************************************************/  
+******************************************************************************/  
 #define __MALLOC_OPTION                    __MALLOC_STD      /* Use standard malloc and free funciton  */
 
 
 #ifdef __DEBUG_ENABLE
 #define __DEBUG_MODE_MAIN                  __DEBUG_BASIC_INFO
-#define __DEBUG_MODE_MOE                   __DEBUG_NONE  //__DEBUG_BASIC_INFO
-#define __DEBUG_MODE_MOE_APP               __DEBUG_NONE  //__DEBUG_BASIC_INFO
-#define __DEBUG_MODE_MOE_TIMER             __DEBUG_NONE  //__DEBUG_NONE  
-#define __DEBUG_MODE_MOE_LINK_LIST         __DEBUG_BASIC_INFO  //__DEBUG_BASIC_INFO
-#define __DEBUG_MODE_MOE_QUEUE             __DEBUG_BASIC_INFO  //__DEBUG_BASIC_INFO
-#define __DEBUG_MODE_MOE_MSG               __DEBUG_NONE  //__DEBUG_BASIC_INFO
-#define __DEBUG_MODE_TASK_LED_DEMO         __DEBUG_BASIC_INFO//__DEBUG_FILE_LINE_INFO 
-#define __DEBUG_MODE_APP_TASK_1            __DEBUG_NONE//__DEBUG_BASIC_INFO
-#define __DEBUG_MODE_APP_TASK_2            __DEBUG_BASIC_INFO//__DEBUG_FUNC_LINE_INFO 
-#define __DEBUG_MODE_APP_TASK_3            __DEBUG_BASIC_INFO//__DEBUG_FILE_LINE_FUNC_INFO 
-#define __DEBUG_MODE_APP_TASK__DEMO_QUEUE  __DEBUG_BASIC_INFO//__DEBUG_FILE_LINE_FUNC_INFO 
-#define __DEBUG_MODE_MOE_EVENT             __DEBUG_NONE//__DEBUG_FILE_LINE_FUNC_INFO 
+#define __DEBUG_MODE_MOE                   __DEBUG_NONE  
+#define __DEBUG_MODE_MOE_APP               __DEBUG_NONE  
+#define __DEBUG_MODE_MOE_TIMER             __DEBUG_NONE  
+#define __DEBUG_MODE_MOE_LINK_LIST         __DEBUG_BASIC_INFO  
+#define __DEBUG_MODE_MOE_QUEUE             __DEBUG_BASIC_INFO  
+#define __DEBUG_MODE_MOE_MSG               __DEBUG_NONE  
+#define __DEBUG_MODE_TASK_LED_DEMO         __DEBUG_BASIC_INFO
+#define __DEBUG_MODE_APP_TASK_1            __DEBUG_NONE
+#define __DEBUG_MODE_APP_TASK_2            __DEBUG_BASIC_INFO
+#define __DEBUG_MODE_APP_TASK_3            __DEBUG_BASIC_INFO
+#define __DEBUG_MODE_APP_TASK__DEMO_QUEUE  __DEBUG_BASIC_INFO
+#define __DEBUG_MODE_MOE_EVENT             __DEBUG_NONE
 #define __DEBUG_MODE_BOARD_CONFIG          __DEBUG_BASIC_INFO
 #define __DEBUG_MODE_MOE_HAL               __DEBUG_BASIC_INFO
 #define __DEBUG_MODE_PT_DEMO               __DEBUG_BASIC_INFO
@@ -120,8 +117,8 @@ Options for __MALLOC_OPTION:
 
                            
 #if (__MALLOC_OPTION == __MALLOC_MY)         /* If you want to use your malloc and free */
-#define MOE_MALLOC(size)   My_Malloc(size)  /* Add your malloc function here           */
-#define MOE_FREE(p)        My_Free(p)       /* Add your free function here             */
+#define MOE_MALLOC(size)   My_Malloc(size)   /* Add your malloc function here           */
+#define MOE_FREE(p)        My_Free(p)        /* Add your free function here             */
 #endif
 
 void LED_Off(uint8 u8Ch);
