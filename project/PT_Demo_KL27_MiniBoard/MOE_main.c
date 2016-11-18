752e72594fb1c618bb95e6b947b1b7e1f2848193
Ian Hom <ianhom@163.com>
2016-11-18 10:06:28 +0800

    Create MOE_main.c

---
+++ project/PT_Demo_KL27_MiniBoard/MOE_main.c
@@ -0,0 +1,54 @@
+/******************************************************************************
+* File       : Moe_main.c
+* Function   : Main-start of the project.
+* description: To be done.          
+* Version    : V1.00
+* Author     : Ian
+* Date       : 4th Nov 2016
+* History    :  No.  When           Who           What
+*               1    13/Nov/2016    Ian           Create
+******************************************************************************/
+#include "common.h"
+#include "common_head.h"
+#include "project_config.h"
+#include "Moe_main.h"
+#include "debug.h"
+#include "MOE_Core.h"
+#include "MOE_Timer.h"
+#include "MOE_Msg.h"
+#include "Board_Config.h"
+ 
+/********************************************************************/
+
+__root const char cg_au8Version[] = MOE_VERSION" ("__DATE__" - " __TIME__ ")";
+
+
+int main (void)
+{      
+    uint8 u8Idx;
+
+    Gpio_Init();     /* Init GPIOs */
+    Timer_Init();    /* Init Timer */
+
+    /* Print all loaded tasks */
+    DBG_PRINT("\n***** %s *****\n", cg_au8Version);
+    DBG_PRINT("Project: %s:\n", MOE_PROJECT_NAME);
+    DBG_PRINT("Task count is %d:\n",MAX_TASK_NUM);
+    for(u8Idx = 0; u8Idx < MAX_TASK_NUM; u8Idx++)
+    {
+        DBG_PRINT("---Task No. is %3d : %s.\n", (u8Idx+1),cg_apu8TaskName[u8Idx]);
+    }
+    DBG_PRINT("\n\n");
+
+    /* Init MOE  */
+    Moe_Init(App_GetSystemTime_ms, NULL);   
+
+    /* Run MOE */
+    Moe_Run();
+    
+    /* Never reach here */
+    return 0;
+}
+
+/* End of file */
+
