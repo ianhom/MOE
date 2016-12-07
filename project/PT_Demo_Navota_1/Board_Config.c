d7e9e0fc2f760041132f20dc8c8c31797da7b713
Ian Hom <ianhom@163.com>
2016-12-07 20:10:28 +0800

    Create Board_Config.c

---
+++ project/PT_Demo_Navota_1/Board_Config.c
@@ -0,0 +1,128 @@
+/******************************************************************************
+* File       : Board_Config.c
+* Function   : Hardware configurations
+* description: Used in PT_Demo_Navota_1 project.           
+* Version    : V1.00
+* Author     : Ian
+* Date       : 7th Dec 2016
+* History    :  No.  When           Who           What
+*               1    7th/Dec/2016   Ian           Create
+******************************************************************************/
+
+#include "common.h"
+#include "common_head.h"
+#include "project_config.h"
+#include "MOE_Core.h"
+#include "MOE_Event.h"
+#include "Board_Config.h"
+#include "debug.h"
+
+
+/******************************************************************************
+* Name       : void Gpio_Init(void)
+* Function   : Init GPIOs
+* Input      : None
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 7th Dec 2016
+******************************************************************************/
+void Gpio_Init(void)
+{
+    /* Led configure */
+    PORTC_PCR8  = PORT_PCR_MUX(0x1);   /* Configure as GPIO              */
+    GPIOC_PSOR  |= (1<<8);             /* Turn off first                 */
+    GPIOC_PDDR  |= (1<<8);             /* Configure as output LED BLUE   */
+
+    return;
+}
+
+/******************************************************************************
+* Name       : void LED_Toggle(uint8 u8Ch)
+* Function   : LED toggle control
+* Input      : uint8 u8Ch    0~255    Channel number of led
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 7th Dec 2016
+******************************************************************************/
+void LED_Toggle(uint8 u8Ch)
+{
+    switch(u8Ch)
+    { 
+        case BOARD_CONFIG_LED_BLUE:  /* If it is blue led   */
+        {
+            GPIOC_PTOR |= (1<<8);
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+    return;
+}
+
+/******************************************************************************
+* Name       : void LED_Off(uint8 u8Ch)
+* Function   : LED Off control
+* Input      : uint8 u8Ch    0~255    Channel number of led
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 7th Dec 2016
+******************************************************************************/
+void LED_Off(uint8 u8Ch)
+{
+    switch(u8Ch)
+    { 
+        case BOARD_CONFIG_LED_BLUE:  /* If it is blue led   */
+        {
+            GPIOC_PSOR |= (1<<8);
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+    return;
+}
+
+
+/******************************************************************************
+* Name       : void LED_On(uint8 u8Ch)
+* Function   : LED On control
+* Input      : uint8 u8Ch    0~255    Channel number of led
+* Output:    : None
+* Return     : None
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 7th Dec 2016
+******************************************************************************/
+void LED_On(uint8 u8Ch)
+{
+    switch(u8Ch)
+    { 
+        case BOARD_CONFIG_LED_BLUE:  /* If it is blue led   */
+        {
+            GPIOC_PCOR |= (1<<8);
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+    return;
+}
+
+
+/* End of file */
