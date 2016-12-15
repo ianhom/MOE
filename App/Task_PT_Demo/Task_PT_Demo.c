38295d45d54920fa732226046c276e20a4a0e485
Ian Hom <ianhom@163.com>
2016-12-15 21:39:53 +0800

    Update Task_PT_Demo.c

--- App/Task_PT_Demo/Task_PT_Demo.c
+++ App/Task_PT_Demo/Task_PT_Demo.c
@@ -9,16 +9,16 @@
 *               1    08/Jul/2016    Ian           Create
 ******************************************************************************/
 
-#include "type_def.h"
-#include "common_head.h"
+#include "../../Pub/type_def.h"
+#include "../../Pub/common_head.h"
 #include "project_config.h"
-#include "MOE_Core.h"
-#include "MOE_Event.h"
-#include "MOE_Timer.h"
-#include "MOE_Msg.h"
-#include "MOE_PT.h"
-#include "Task_PT_Demo.h"
-#include "debug.h"
+#include "../../Core/MOE_Core.h"
+#include "../../Core/MOE_Event.h"
+#include "../../Core/MOE_Timer.h"
+#include "../../Core/MOE_Msg.h"
+#include "../../Core/MOE_PT.h"
+#include "./Task_PT_Demo.h"
+#include "../../Pub/debug.h"
 
 static uint8 sg_u8TaskID = TASK_NO_TASK;
 
