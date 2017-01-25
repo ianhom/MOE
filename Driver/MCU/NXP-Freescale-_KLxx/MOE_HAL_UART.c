b303fede2a0792decdcf93c9bef70501941d48d6
Ian Hom <ianhom@163.com>
2017-01-25 10:24:28 +0800

    Update MOE_HAL_UART.c

--- Driver/MCU/NXP-Freescale-_KLxx/MOE_HAL_UART.c
+++ Driver/MCU/NXP-Freescale-_KLxx/MOE_HAL_UART.c
@@ -26,7 +26,7 @@ static uint8 sg_au8RcvData[40] = {0};
 static uint8 sg_u8GetTel = MOE_HAL_UART_NO_RCV_TEL;
 
 /******************************************************************************
-* Name       : uint8 Moe_HAL_UART_Cfg(T_MOE_PERIPHERAL_DRV *ptCfg)
+* Name       : uint8 Moe_HAL_UART_Cfg(T_MOE_DRV_PARA *ptCfg)
 * Function   : Configure hardware abstract layer for UART
 * Input      : None.
 * Output:    : None.
@@ -37,7 +37,7 @@ static uint8 sg_u8GetTel = MOE_HAL_UART_NO_RCV_TEL;
 * Author     : Ian
 * Date       : 19th Jul 2016
 ******************************************************************************/
-uint8 Moe_HAL_UART_Cfg(T_MOE_PERIPHERAL_DRV *ptCfg)
+uint8 Moe_HAL_UART_Cfg(T_MOE_DRV_PARA *ptCfg)
 {
     volatile uint8 u8Data = 0;
     uint16         U16SBR = 0; 
@@ -85,7 +85,7 @@ uint8 Moe_HAL_UART_Cfg(T_MOE_PERIPHERAL_DRV *ptCfg)
 }
 
 /******************************************************************************
-* Name       : uint8 Moe_HAL_UART_Ctrl(T_MOE_PERIPHERAL_DRV *ptCtrl)
+* Name       : uint8 Moe_HAL_UART_Ctrl(T_MOE_DRV_PARA *ptCtrl)
 * Function   : Send control command to UART
 * Input      : None.
 * Output:    : None.
@@ -96,7 +96,7 @@ uint8 Moe_HAL_UART_Cfg(T_MOE_PERIPHERAL_DRV *ptCfg)
 * Author     : Ian
 * Date       : 19th Jul 2016
 ******************************************************************************/
-uint8 Moe_HAL_UART_Ctrl(T_MOE_PERIPHERAL_DRV *ptCtrl)
+uint8 Moe_HAL_UART_Ctrl(T_MOE_DRV_PARA *ptCtrl)
 {
     reutrn SW_OK;
 }
