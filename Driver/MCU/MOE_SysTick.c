eb9ea29c56c70263dfb5a23d5ea124d844a6a86e
Ian Hom <ianhom@163.com>
2017-02-16 19:32:17 +0800

    Update MOE_SysTick.c

--- Driver/MCU/MOE_SysTick.c
+++ Driver/MCU/MOE_SysTick.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * File       : MOE_SysTick.c
-* Function   : set SysTick for 1 ms
+* Function   : Set SysTick for 1 ms
 * description: To be done.           
 * Version    : V1.00
 * Author     : Ian
@@ -19,8 +19,20 @@
 #include "../../Pub/debug.h"
 //#include "core_cm0plus.h"
 
+/* System ms counter */
 volatile static uint32 sg_u32SysClk = 0;
 
+/******************************************************************************
+* Name       : void SysTick_Ms_Int_Init(uint32_t u32Tick)
+* Function   : Init SysTick Register
+* Input      : uint32_t u32Tick    1~0xFFFFFFFF     The tick count for 1 ms
+* Output:    : None.
+* Return     : None.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 16th Feb 2017
+******************************************************************************/
 void SysTick_Ms_Int_Init(uint32_t u32Tick)
 {
     SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk;  /* Set clock source = core clock */
@@ -31,16 +43,38 @@ void SysTick_Ms_Int_Init(uint32_t u32Tick)
     return;   
 }
 
+/******************************************************************************
+* Name       : uint32 SysTick_GetSysClk(void)
+* Function   : Return the current system ms counter(system clock)
+* Input      : uint32_t u32Tick    1~0xFFFFFFFF     The tick count for 1 ms
+* Output:    : None.
+* Return     : 1~0xFFFFFFFF   The current system ms counter.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 16th Feb 2017
+******************************************************************************/
 uint32 SysTick_GetSysClk(void)
 {
-    return sg_u32SysClk;
+    return sg_u32SysClk;  /* System ms counter */
 }
 
+/******************************************************************************
+* Name       : void SysTick_Handler(void)
+* Function   : SysTick interrupt handler
+* Input      : None.
+* Output:    : None.
+* Return     : None.
+* description: To be done.
+* Version    : V1.00
+* Author     : Ian
+* Date       : 16th Feb 2017
+******************************************************************************/
 void SysTick_Handler(void)
 {
     volatile uint32 u32Temp;
     u32Temp = SysTick->CTRL;
-    sg_u32SysClk++;
+    sg_u32SysClk++;            /* Update the system ms counter */
     return;
 }
 
