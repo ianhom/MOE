5bfc8ca6f3b9a260c086610008e65f03fd60aa85
Ian Hom <ianhom@163.com>
2017-02-18 16:04:11 +0800

    Update MOE_SysTick.h

--- Driver/MCU/Common/ARM_Cortex_M/MOE_SysTick.h
+++ Driver/MCU/Common/ARM_Cortex_M/MOE_SysTick.h
@@ -26,48 +26,48 @@ extern "C" {
 #define __DEBUG_MODE      __DEBUG_MODE_MOE_SYSTICK   /* According the set from project_config.h */
 #endif
 
-typedef struct
+typedef struct _MOE_SysTick_Type
 {
     volatile uint32_t CTRL;                   /*!< Offset: 0x000 (R/W)  SysTick Control and Status Register */
     uint32_t LOAD;                   /*!< Offset: 0x004 (R/W)  SysTick Reload Value Register */
     uint32_t VAL;                    /*!< Offset: 0x008 (R/W)  SysTick Current Value Register */
     uint32_t CALIB;                  /*!< Offset: 0x00C (R/ )  SysTick Calibration Register */
-} SysTick_Type;
+} MOE_SysTick_Type;
 
 /* SysTick Control / Status Register Definitions */
-#define SysTick_CTRL_COUNTFLAG_Pos         16U                                            /*!< SysTick CTRL: COUNTFLAG Position */
-#define SysTick_CTRL_COUNTFLAG_Msk         (1UL << SysTick_CTRL_COUNTFLAG_Pos)            /*!< SysTick CTRL: COUNTFLAG Mask */
+#define MOE_SysTick_CTRL_COUNTFLAG_POS         16U                                            /*!< SysTick CTRL: COUNTFLAG Position */
+#define MOE_SysTick_CTRL_COUNTFLAG_MSK         (1UL << SysTick_CTRL_COUNTFLAG_POS)            /*!< SysTick CTRL: COUNTFLAG Mask */
 
-#define SysTick_CTRL_CLKSOURCE_Pos          2U                                            /*!< SysTick CTRL: CLKSOURCE Position */
-#define SysTick_CTRL_CLKSOURCE_Msk         (1UL << SysTick_CTRL_CLKSOURCE_Pos)            /*!< SysTick CTRL: CLKSOURCE Mask */
+#define MOE_SysTick_CTRL_CLKSOURCE_POS          2U                                            /*!< SysTick CTRL: CLKSOURCE Position */
+#define MOE_SysTick_CTRL_CLKSOURCE_MSK         (1UL << SysTick_CTRL_CLKSOURCE_POS)            /*!< SysTick CTRL: CLKSOURCE Mask */
 
-#define SysTick_CTRL_TICKINT_Pos            1U                                            /*!< SysTick CTRL: TICKINT Position */
-#define SysTick_CTRL_TICKINT_Msk           (1UL << SysTick_CTRL_TICKINT_Pos)              /*!< SysTick CTRL: TICKINT Mask */
+#define MOE_SysTick_CTRL_TICKINT_POS            1U                                            /*!< SysTick CTRL: TICKINT Position */
+#define MOE_SysTick_CTRL_TICKINT_MSK           (1UL << SysTick_CTRL_TICKINT_POS)              /*!< SysTick CTRL: TICKINT Mask */
 
-#define SysTick_CTRL_ENABLE_Pos             0U                                            /*!< SysTick CTRL: ENABLE Position */
-#define SysTick_CTRL_ENABLE_Msk            (1UL /*<< SysTick_CTRL_ENABLE_Pos*/)           /*!< SysTick CTRL: ENABLE Mask */
+#define MOE_SysTick_CTRL_ENABLE_POS             0U                                            /*!< SysTick CTRL: ENABLE Position */
+#define MOE_SysTick_CTRL_ENABLE_MSK            (1UL /*<< SysTick_CTRL_ENABLE_Pos*/)           /*!< SysTick CTRL: ENABLE Mask */
 
 /* SysTick Reload Register Definitions */
-#define SysTick_LOAD_RELOAD_Pos             0U                                            /*!< SysTick LOAD: RELOAD Position */
-#define SysTick_LOAD_RELOAD_Msk            (0xFFFFFFUL /*<< SysTick_LOAD_RELOAD_Pos*/)    /*!< SysTick LOAD: RELOAD Mask */
+#define MOE_SysTick_LOAD_RELOAD_POS             0U                                            /*!< SysTick LOAD: RELOAD Position */
+#define MOE_SysTick_LOAD_RELOAD_MSK            (0xFFFFFFUL /*<< SysTick_LOAD_RELOAD_Pos*/)    /*!< SysTick LOAD: RELOAD Mask */
 
 /* SysTick Current Register Definitions */
-#define SysTick_VAL_CURRENT_Pos             0U                                            /*!< SysTick VAL: CURRENT Position */
-#define SysTick_VAL_CURRENT_Msk            (0xFFFFFFUL /*<< SysTick_VAL_CURRENT_Pos*/)    /*!< SysTick VAL: CURRENT Mask */
+#define MOE_SysTick_VAL_CURRENT_POS             0U                                            /*!< SysTick VAL: CURRENT Position */
+#define MOE_SysTick_VAL_CURRENT_MSK            (0xFFFFFFUL /*<< SysTick_VAL_CURRENT_Pos*/)    /*!< SysTick VAL: CURRENT Mask */
 
 /* SysTick Calibration Register Definitions */
-#define SysTick_CALIB_NOREF_Pos            31U                                            /*!< SysTick CALIB: NOREF Position */
-#define SysTick_CALIB_NOREF_Msk            (1UL << SysTick_CALIB_NOREF_Pos)               /*!< SysTick CALIB: NOREF Mask */
+#define MOE_SysTick_CALIB_NOREF_POS            31U                                            /*!< SysTick CALIB: NOREF Position */
+#define MOE_SysTick_CALIB_NOREF_MSK            (1UL << SysTick_CALIB_NOREF_POS)               /*!< SysTick CALIB: NOREF Mask */
 
-#define SysTick_CALIB_SKEW_Pos             30U                                            /*!< SysTick CALIB: SKEW Position */
-#define SysTick_CALIB_SKEW_Msk             (1UL << SysTick_CALIB_SKEW_Pos)                /*!< SysTick CALIB: SKEW Mask */
+#define MOE_SysTick_CALIB_SKEW_POS             30U                                            /*!< SysTick CALIB: SKEW Position */
+#define MOE_SysTick_CALIB_SKEW_MSK             (1UL << SysTick_CALIB_SKEW_POS)                /*!< SysTick CALIB: SKEW Mask */
 
-#define SysTick_CALIB_TENMS_Pos             0U                                            /*!< SysTick CALIB: TENMS Position */
-#define SysTick_CALIB_TENMS_Msk            (0xFFFFFFUL /*<< SysTick_CALIB_TENMS_Pos*/)    /*!< SysTick CALIB: TENMS Mask */
+#define MOE_SysTick_CALIB_TENMS_POS             0U                                            /*!< SysTick CALIB: TENMS Position */
+#define MOE_SysTick_CALIB_TENMS_MSK            (0xFFFFFFUL /*<< SysTick_CALIB_TENMS_Pos*/)    /*!< SysTick CALIB: TENMS Mask */
 
-#define SysTick_BASE        (SCS_BASE +  0x0010UL)                    /*!< SysTick Base Address */
+#define MOE_SysTick_BASE        (SCS_BASE +  0x0010UL)                    /*!< SysTick Base Address */
 
-#define SysTick             ((SysTick_Type   *)     SysTick_BASE  )   /*!< SysTick configuration struct */
+#define MOE_SysTick             ((MOE_SysTick_Type   *)     MOE_SysTick_BASE  )   /*!< SysTick configuration struct */
 
 
 /******************************************************************************
