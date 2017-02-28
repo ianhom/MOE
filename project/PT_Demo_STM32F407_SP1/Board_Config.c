b0d09430daca22d3ecb34fc295a56c4b08420d47
Ian Hom <ianhom@163.com>
2017-02-28 17:56:00 +0800

    Update Board_Config.c

--- project/PT_Demo_STM32F407_SP1/Board_Config.c
+++ project/PT_Demo_STM32F407_SP1/Board_Config.c
@@ -17,9 +17,8 @@
 #include "../../Core/MOE_Msg.h"
 #include "./Board_Config.h"
 #include "../../Pub/debug.h"
+#include "../../Driver/MCU/Common/ARM_Cortex_M/MOE_SysTick.h"
 #include "stm32f4xx_hal.h"
-//#include "core_cm0plus.h"
-//#include "stm32l0xx_hal.h"
 
 UART_HandleTypeDef huart;
 
@@ -56,29 +55,6 @@ void Board_Init(void)
     MX_USART_UART_Init();
 
     SysTick_Ms_Int_Init(SYSTICK_CNT_FOR_1_MS);
-    return;
-}
-
-void SysTick_Ms_Int_Init(uint32_t u32Tick)
-{
-    SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk;  /* Set clock source = core clock */
-    SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;    /* Enable systick interrupt      */
-    SysTick->LOAD  = u32Tick;                     /* Set tick count for 1ms        */
-    SysTick->VAL   = 0;
-    SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;     /* Enable SysTick                */
-    return;   
-}
-
-uint32 SysTick_GetSysClk(void)
-{
-    return sg_u32SysClk;
-}
-
-void SysTick_Handler(void)
-{
-    volatile uint32 u32Temp;
-    u32Temp = SysTick->CTRL;
-    sg_u32SysClk++;
     return;
 }
 
