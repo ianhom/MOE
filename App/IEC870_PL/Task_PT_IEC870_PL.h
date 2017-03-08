a57f68f76dec67d06705e308ece389367b9eb20c
Ian Hom <ianhom@163.com>
2017-03-08 22:36:27 +0800

    Update Task_PT_IEC870_PL.h

--- App/IEC870_PL/Task_PT_IEC870_PL.h
+++ App/IEC870_PL/Task_PT_IEC870_PL.h
@@ -1,7 +1,7 @@
 /******************************************************************************
 * File       : Task_PT_IEC870_PL.h
 * Function   : A demo task for PT application
-* description: To be done.           
+* Description: To be done.           
 * Version    : V1.00
 * Author     : Ian
 * Date       : 8th Jul 2016
@@ -66,7 +66,7 @@ extern "C" {
 * Return     : SW_OK   Successful operation
 *            : SW_ERR  Failed operation
 *              1~254   Event which is not processed.
-* description: To be done
+* Description: To be done
 * Version    : V1.00
 * Author     : Ian
 * Date       : 13th Jul 2016
