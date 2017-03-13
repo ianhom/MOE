0f1ca4f2146fa0556bce40fe1812ec010a40d1ed
Ian Hom <ianhom@163.com>
2017-03-13 08:16:21 +0800

    Update Task2.h

--- App/Task2/Task2.h
+++ App/Task2/Task2.h
@@ -1,7 +1,7 @@
 /******************************************************************************
 * File       : Task2.h
 * Function   : To be doen.
-* description: To be done.          
+* Description: To be done.          
 * Version    : V1.00
 * Author     : Ian
 * Date       : 3rd May 2016
@@ -37,7 +37,7 @@ extern "C" {
 * Return     : SW_OK   Successful operation
 *            : SW_ERR  Failed operation
 *              1~254   Event which is not processed.
-* description: To be done
+* Description: To be done
 * Version    : V1.00
 * Author     : Ian
 * Date       : 3rd May 2016
