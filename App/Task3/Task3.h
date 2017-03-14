d1344c1bab4b4e16267017e8eb695ee830f69820
Ian Hom <ianhom@163.com>
2017-03-14 18:53:29 +0800

    Update Task3.h

--- App/Task3/Task3.h
+++ App/Task3/Task3.h
@@ -1,7 +1,7 @@
 /******************************************************************************
 * File       : Task3.h
 * Function   : To be done.
-* description: To be done.          
+* Description: To be done.          
 * Version    : V1.00
 * Author     : Ian
 * Date       : 3rd May 2016
@@ -38,7 +38,7 @@ extern "C" {
 * Return     : SW_OK   Successful operation
 *            : SW_ERR  Failed operation
 *              1~254   Event which is not processed.
-* description: To be done
+* Description: To be done
 * Version    : V1.00
 * Author     : Ian
 * Date       : 3rd May 2016
