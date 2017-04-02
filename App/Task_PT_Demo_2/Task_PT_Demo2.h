e64b7081e76c94f51f40290f9462cad62a887fba
Ian Hom <ianhom@163.com>
2017-04-02 18:14:34 +0800

    Update Task_PT_Demo2.h

--- App/Task_PT_Demo_2/Task_PT_Demo2.h
+++ App/Task_PT_Demo_2/Task_PT_Demo2.h
@@ -1,7 +1,7 @@
 /******************************************************************************
 * File       : Task_PT_Demo2.h
 * Function   : Another demo task for PT application
-* description: To be done.           
+* Description: To be done.           
 * Version    : V1.00
 * Author     : Ian
 * Date       : 8th Jul 2016
@@ -38,7 +38,7 @@ extern "C" {
 * Return     : SW_OK   Successful operation
 *            : SW_ERR  Failed operation
 *              1~254   Event which is not processed.
-* description: To be done
+* Description: To be done
 * Version    : V1.00
 * Author     : Ian
 * Date       : 8th Jul 2016
