9cd9a035f896e57cd1136514f4d41bccbd6edb0c
A Byte of China <ianhom@163.com>
2017-11-29 19:34:40 +0800

    Create main.h

---
+++ project/PT_Demo_MIMXRT1050-EVK/main.h
@@ -0,0 +1,38 @@
+/******************************************************************************
+* File       : Moe_main.h
+* Function   : Main-start of the project.
+* Description: To be done.          
+* Version    : V1.00
+* Author     : Ian
+* Date       : 29th Nov 2017
+* History    :  No.  When           Who           What
+*               1    29/Nov/2019    Ian           Create
+******************************************************************************/
+
+#ifndef _MOE_MAIN_H_
+#define _MOE_MAIN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Check if specified option is set for debugging */
+#ifndef __DEBUG_MODE_MAIN                    
+#define __DEBUG_MODE      __DEBUG_NONE                /* Default: None debugging info            */
+#else
+#ifdef __DEBUG_MODE
+#undef __DEBUG_MODE
+#endif
+#define __DEBUG_MODE      __DEBUG_MODE_MAIN           /* According the set from project_config.h */
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _MOE_MAIN_H */
+
+/* End of file */
+
